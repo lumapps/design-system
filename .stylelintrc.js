1202793e38970589c9e517f6a9cbb8ec7be7dc84
Matthias Manoukian <matthiasmanoukian@MacBook-Pro-de-Matthias.local>
2019-02-07 15:58:10 +0100

    chore: typo stylelintrc

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,4 @@
-modules.exports = {
+module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss', 'stylelint-config-idiomatic-order'],
     rules: {
         indentation: 4,
