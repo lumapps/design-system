0c7aefa32c9f4b9e1246ece9d763ab34181052a6
Clément PREVOT <clement@lumapps.com>
2019-03-13 17:51:53 +0100

    chore(linter): fix the usage of prettier-tslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,7 +5,7 @@ module.exports = {
     ignore: ['**/dist/*', '**/build/*'],
     linters: {
         '*.{js,jsx}': ['prettier-eslint --prettier-last --write', 'git add', 'eslint'],
-        '*.{ts,tsx}': ['prettier-tslint --prettier-last --write', 'git add', 'tslint'],
+        '*.{ts,tsx}': ['prettier-tslint fix', 'git add', 'tslint'],
         '*.css': ['prettier-stylelint --prettier-last --write', 'git add', 'stylelint'],
         '*.scss': ['prettier-stylelint --prettier-last --write', 'git add', 'stylelint --syntax=scss'],
         '*.{md,html}': ['prettier --write', 'git add'],
