e50d478da1d2e75dfec72c80757a1d5f7cf766fc
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-04-09 11:47:04 +0200

    feat(lightbox): add component

---
+++ demo/angularjs/components/lightbox/controller.js
@@ -0,0 +1,52 @@
+function DemoLightboxController(LumXLightboxService) {
+    'ngInject';
+
+    const vm = this;
+
+    /////////////////////////////
+    //                         //
+    //    Public attributes    //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * The lightbox id.
+     *
+     * @type {string}
+     */
+    vm.lightboxId = 'lightbox-demo';
+
+    /**
+     * The source button id.
+     *
+     * @type {string}
+     */
+    vm.sourceId = 'lightbox-demo-source';
+
+    /////////////////////////////
+    //                         //
+    //     Public functions    //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * Open the lightbox.
+     */
+    function openLightbox() {
+        LumXLightboxService.open(vm.lightboxId, {
+            source: `#${vm.sourceId}`,
+        });
+    }
+
+    /////////////////////////////
+
+    vm.openLightbox = openLightbox;
+}
+
+/////////////////////////////
+
+angular.module('design-system').controller('DemoLightboxController', DemoLightboxController);
+
+/////////////////////////////
+
+export { DemoLightboxController };
---
+++ src/components/lightbox/angularjs/lightbox_service.js
@@ -0,0 +1,47 @@
+import { COMPONENT_PREFIX, MODULE_NAME, SERVICE_PREFIX } from 'LumX/angularjs/constants/common_constants';
+
+/////////////////////////////
+
+function LightboxService($rootScope) {
+    'ngInject';
+
+    const service = this;
+
+    /////////////////////////////
+    //                         //
+    //     Public functions    //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * Close a given lightbox.
+     *
+     * @param {string} lightboxId The lightbox identifier.
+     */
+    function closeLightbox(lightboxId) {
+        $rootScope.$broadcast(`${COMPONENT_PREFIX}-lightbox__close`, lightboxId);
+    }
+
+    /**
+     * Open a given lightbox.
+     *
+     * @param {string} lightboxId The lightbox identifier.
+     * @param {Object} params     An optional object that holds extra parameters.
+     */
+    function openLightbox(lightboxId, params) {
+        $rootScope.$broadcast(`${COMPONENT_PREFIX}-lightbox__open`, lightboxId, params);
+    }
+
+    /////////////////////////////
+
+    service.close = closeLightbox;
+    service.open = openLightbox;
+}
+
+/////////////////////////////
+
+angular.module(`${MODULE_NAME}.lightbox`).service(`${SERVICE_PREFIX}LightboxService`, LightboxService);
+
+/////////////////////////////
+
+export { LightboxService };
