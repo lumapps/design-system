9cbb5d5d4ca3a891374691d4b0229d33c5b096d1
Florian Nourrisse <florian.nourrisse@lumapps.com>
2019-04-26 17:05:38 +0200

    feat(slideshow): integrate demo

--- demo/react/components/slideshow/default.tsx
+++ demo/react/components/slideshow/default.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
+import React, { CSSProperties } from 'react';
 
-import { Slideshow, Theme } from 'LumX';
+import { ImageBlock, ImageBlockCaptionPositions, ImageBlockProps, Slideshow, Theme, ThumbnailAspectRatios } from 'LumX';
 import { SlideshowItem } from 'LumX/components/slideshow/react/SlideshowItem';
 
 /////////////////////////////
@@ -14,64 +14,93 @@ interface IProps {
 
 /////////////////////////////
 
+const slideshowWrapperStyle: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'center',
+};
+
+const slideshowStyle: CSSProperties = {
+    width: '50%',
+};
+
+const imageBlockDemoProps: Partial<ImageBlockProps> = {
+    description: 'What an image',
+    tags: ['#tag1', '#tag2', '#tag3'],
+    title: 'Nice Image',
+};
+
 /**
  * The demo for the default <Slideshow>s.
  *
  * @return {React.ReactElement} The demo component.
  */
 const DemoComponent: React.FC<IProps> = ({ theme }: IProps): React.ReactElement => (
-    <Fragment>
-        <Slideshow activeIndex={6} hasControls={true} theme={theme} autoPlay={true} groupBy={1}>
+    <div style={slideshowWrapperStyle}>
+        <Slideshow activeIndex={3} hasControls={true} theme={theme} autoPlay={true} groupBy={1} style={slideshowStyle}>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=24" alt="Pic Sum" />
+                <ImageBlock theme={theme} image="https://picsum.photos/640/480/?image=23" />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=25" alt="Pic Sum" />
+                <ImageBlock
+                    aspectRatio={ThumbnailAspectRatios.vertical}
+                    theme={theme}
+                    image="https://picsum.photos/640/480/?image=24"
+                />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=26" alt="Pic Sum" />
+                <ImageBlock
+                    aspectRatio={ThumbnailAspectRatios.horizontal}
+                    theme={theme}
+                    image="https://picsum.photos/640/480/?image=25"
+                />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=27" alt="Pic Sum" />
+                <ImageBlock theme={theme} image="https://picsum.photos/640/480/?image=26" {...imageBlockDemoProps} />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=28" alt="Pic Sum" />
+                <ImageBlock
+                    aspectRatio={ThumbnailAspectRatios.vertical}
+                    theme={theme}
+                    image="https://picsum.photos/640/480/?image=27"
+                    {...imageBlockDemoProps}
+                />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=29" alt="Pic Sum" />
+                <ImageBlock
+                    aspectRatio={ThumbnailAspectRatios.horizontal}
+                    theme={theme}
+                    image="https://picsum.photos/640/480/?image=28"
+                    {...imageBlockDemoProps}
+                />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=30" alt="Pic Sum" />
+                <ImageBlock
+                    captionPosition={ImageBlockCaptionPositions.over}
+                    image="https://picsum.photos/640/480/?image=29"
+                    theme={theme}
+                    {...imageBlockDemoProps}
+                />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=31" alt="Pic Sum" />
+                <ImageBlock
+                    aspectRatio={ThumbnailAspectRatios.vertical}
+                    captionPosition={ImageBlockCaptionPositions.over}
+                    image="https://picsum.photos/640/480/?image=30"
+                    theme={theme}
+                    {...imageBlockDemoProps}
+                />
             </SlideshowItem>
             <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=32" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=33" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=34" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=35" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=36" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=37" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=38" alt="Pic Sum" />
-            </SlideshowItem>
-            <SlideshowItem>
-                <img src="https://picsum.photos/640/480/?image=39" alt="Pic Sum" />
+                <ImageBlock
+                    aspectRatio={ThumbnailAspectRatios.horizontal}
+                    captionPosition={ImageBlockCaptionPositions.over}
+                    image="https://picsum.photos/640/480/?image=31"
+                    theme={theme}
+                    {...imageBlockDemoProps}
+                />
             </SlideshowItem>
         </Slideshow>
-    </Fragment>
+    </div>
 );
 
 /////////////////////////////
