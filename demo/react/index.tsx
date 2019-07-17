8870cfcc1db9a883a1772991c06518899f68b842
pidupuis <pierre@lumapps.com>
2019-07-17 14:05:19 +0200

    docs: add example using jsx in markdown

--- demo/react/index.tsx
+++ demo/react/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import { App } from './App';
+import { AppWithMarkdown } from './AppWithMarkdown';
 
 /////////////////////////////
 
-render(<App />, document.getElementById('root'));
+render(<AppWithMarkdown />, document.getElementById('root'));
