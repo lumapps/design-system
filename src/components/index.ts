e36a078950a21876f00ed9f02cfb0ff5e05b9957
gcornut <guillaume.cornut@lumapps.com>
2019-08-07 18:51:42 +0200

    feat(side-navigation): add react component

--- src/components/index.ts
+++ src/components/index.ts
@@ -54,6 +54,12 @@ enum Orientation {
     vertical = 'vertical',
 }
 
+enum Emphasis {
+    low = 'low',
+    medium = 'medium',
+    high = 'high',
+}
+
 /////////////////////////////
 
-export { Alignment, ComplexPropDefault, Color, ColorPalette, Theme, Size, Orientation };
+export { Alignment, ComplexPropDefault, Color, ColorPalette, Theme, Size, Orientation, Emphasis };
