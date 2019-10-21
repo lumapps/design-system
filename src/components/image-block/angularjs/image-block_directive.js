db8bc7cefa8e8d5014831fe5bd553c2ca82189c4
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-10-21 08:48:17 +0200

    fix(image-block): use tags slot instead of tags prop

--- src/components/image-block/angularjs/image-block_directive.js
+++ src/components/image-block/angularjs/image-block_directive.js
@@ -7,15 +7,35 @@ import template from './image-block.html';
 function ImageBlockController() {
     // eslint-disable-next-line consistent-this, no-unused-vars
     const lumx = this;
+
+    /////////////////////////////
+    //                         //
+    //    Public attributes    //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * Whether the directive has tags slot filled or not.
+     *
+     * @type {boolean}
+     */
+    lumx.hasTags = false;
 }
 
 /////////////////////////////
 
 function ImageBlockDirective() {
+    function link(scope, el, attrs, ctrl, transclude) {
+        if (transclude.isSlotFilled('tags')) {
+            ctrl.hasTags = true;
+        }
+    }
+
     return {
         bindToController: true,
         controller: ImageBlockController,
         controllerAs: 'lumx',
+        link,
         replace: true,
         restrict: 'E',
         scope: {
@@ -27,11 +47,13 @@ function ImageBlockDirective() {
             fillHeight: '=?lumxFillHeight',
             image: '@lumxImage',
             onClick: '&?lumxOnClick',
-            tags: '=?lumxTags',
             theme: '@?lumxTheme',
             title: '@?lumxTitle',
         },
         template,
+        transclude: {
+            tags: `?${COMPONENT_PREFIX}ImageBlockTags`,
+        },
     };
 }
 
