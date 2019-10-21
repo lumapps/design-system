3df1a17636e780d8610032ef818a6bdd5bde9c1b
gcornut <guillaume.cornut@lumapps.com>
2019-10-21 11:01:34 +0200

    feat(demo): full rework of the demo site build
    - Create new static website build
    - Rework the demo site dev server

--- webpack/react/webpack.config.js
+++ webpack/react/webpack.config.js
@@ -4,7 +4,6 @@ const merge = require('webpack-merge');
 
 const { SRC_PATH } = require('../constants');
 
-const { babelSetup } = require('../utils');
 const baseConfig = require('../webpack.config');
 
 const reactConfig = {
@@ -18,37 +17,9 @@ const reactConfig = {
             '@lumx/react': path.resolve(__dirname, `${SRC_PATH}/react.index.ts`),
         },
     },
-
-    resolveLoader: {
-        alias: {
-            'props-loader': path.resolve(__dirname, 'props-loader'),
-            'mdx-loader': path.resolve(__dirname, 'mdx-loader'),
-        },
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.(mdx|md)?$/,
-                use: [
-                    {
-                        loader: 'babel-loader?cacheDirectory=true',
-                        options: babelSetup({
-                            presets: ['@babel/preset-react'],
-                        }),
-                    },
-                    {
-                        loader: 'mdx-loader',
-                    },
-                ],
-            },
-        ],
-    },
 };
 
 module.exports = merge.smartStrategy({
     entry: 'append',
-    'module.rules': 'append',
     'resolve.alias': 'append',
-    'resolveLoader.alias': 'append',
 })(baseConfig, reactConfig);
