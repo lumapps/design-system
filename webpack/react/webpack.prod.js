85e0ccac519c496a2e30eb35a0998e94e17b8615
gcornut <guillaume.cornut@lumapps.com>
2019-10-30 11:26:17 +0100

    feat(build): rework react build:prod with TypeScript declaration file

--- webpack/react/webpack.prod.js
+++ webpack/react/webpack.prod.js
@@ -8,6 +8,8 @@ const HtmlMinifierPlugin = require('html-minifier-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const TsDeclarationWebpackPlugin = require('ts-declaration-webpack-plugin');
+const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
 const { getStyleLoader } = require('../utils');
@@ -15,18 +17,105 @@ const { COMPONENTS_PATH, CONFIGS, CORE_PATH, DIST_PATH, EXAMPLES_PATH, ROOT_PATH
 
 const reactConfig = require('./webpack.config');
 
-const minify = Boolean(process.env.MINIFY);
-const generatePackage = Boolean(process.env.GENERATE_PACKAGE);
-
-const filename = `[name]${minify ? '.min' : ''}`;
+const filename = '[name].min';
 const distTechPath = `${DIST_PATH}/react`;
 
-const minimizer = [];
+const minimizer = [
+    new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: CONFIGS.terser,
+    }),
+];
+
 const plugins = [
     ...reactConfig.plugins,
+
+    /* Extract CSS files */
     new ExtractCssChunks({
         chunkFilename: `${filename}.css`,
         filename: `${filename}.css`,
+    }),
+
+    /* Bundle static files */
+    new CopyWebpackPlugin([
+        {
+            from: `${EXAMPLES_PATH}/react/`,
+            to: `${DIST_PATH}/react/examples/`,
+        },
+        {
+            from: `${EXAMPLES_PATH}/styles.css`,
+            to: `${DIST_PATH}/react/examples/`,
+        },
+        {
+            from: `${ROOT_PATH}/CONTRIBUTING.md`,
+            to: `${DIST_PATH}/react/`,
+        },
+        {
+            from: `${ROOT_PATH}/LICENSE.md`,
+            to: `${DIST_PATH}/react/`,
+        },
+        {
+            from: `${CORE_PATH}/react/README.md`,
+            to: `${DIST_PATH}/react/`,
+        },
+        {
+            from: `${CORE_PATH}/react/package.json`,
+            to: `${DIST_PATH}/react/`,
+        },
+        {
+            context: `${CORE_PATH}/style/`,
+            from: `${CORE_PATH}/style/*.scss`,
+            to: `${DIST_PATH}/react/scss`,
+        },
+        {
+            context: `${CORE_PATH}/style/`,
+            from: { glob: `${CORE_PATH}/style/**/*.scss`, ignore: [`${CORE_PATH}/style/*`] },
+            to: `${DIST_PATH}/react/scss/core`,
+        },
+        {
+            context: `${COMPONENTS_PATH}/`,
+            from: `${COMPONENTS_PATH}/**/style/**/*.scss`,
+            to: `${DIST_PATH}/react/scss/components`,
+            transformPath: (targetPath) => targetPath.replace('/style/', '/'),
+        },
+    ]),
+
+    /* Fix SCSS paths */
+    new ReplaceInFileWebpackPlugin([
+        {
+            dir: `${DIST_PATH}/react/scss`,
+            rules: [
+                {
+                    search: /'\.\/(base|elevation|grid|input|link|size|spacing|state|theme|typography)/g,
+                    replace: "'./core/$1",
+                },
+                {
+                    search: /'\.\.\/\.\.\/components/g,
+                    replace: "'./components",
+                },
+                {
+                    search: /\/style/g,
+                    replace: '',
+                },
+            ],
+        },
+    ]),
+
+    /* Minimize HTML & CSS */
+    new HtmlMinifierPlugin(CONFIGS.htmlMinifier),
+    new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: CONFIGS.cssNano,
+        cssProcessorPluginOptions: {},
+    }),
+
+    /* Bundle unminified JS */
+    new UnminifiedWebpackPlugin(),
+
+    /* Bundle TypeScript declaration file */
+    new TsDeclarationWebpackPlugin({
+        name: 'lumx.react.d.ts',
     }),
 ];
 
@@ -34,93 +123,7 @@ if (!IS_CI) {
     plugins.push(
         new WebpackNotifierPlugin({
             alwaysNotify: true,
-            title: `LumX - React - ${minify ? 'Minified package' : 'Package'}`,
-        }),
-    );
-}
-
-if (generatePackage) {
-    plugins.push(
-        new CopyWebpackPlugin([
-            {
-                from: `${EXAMPLES_PATH}/react/`,
-                to: `${DIST_PATH}/react/examples/`,
-            },
-            {
-                from: `${EXAMPLES_PATH}/styles.css`,
-                to: `${DIST_PATH}/react/examples/`,
-            },
-            {
-                from: `${ROOT_PATH}/CONTRIBUTING.md`,
-                to: `${DIST_PATH}/react/`,
-            },
-            {
-                from: `${ROOT_PATH}/LICENSE.md`,
-                to: `${DIST_PATH}/react/`,
-            },
-            {
-                from: `${CORE_PATH}/react/README.md`,
-                to: `${DIST_PATH}/react/`,
-            },
-            {
-                from: `${CORE_PATH}/react/package.json`,
-                to: `${DIST_PATH}/react/`,
-            },
-            {
-                context: `${CORE_PATH}/style/`,
-                from: `${CORE_PATH}/style/*.scss`,
-                to: `${DIST_PATH}/react/scss`,
-            },
-            {
-                context: `${CORE_PATH}/style/`,
-                from: { glob: `${CORE_PATH}/style/**/*.scss`, ignore: [`${CORE_PATH}/style/*`] },
-                to: `${DIST_PATH}/react/scss/core`,
-            },
-            {
-                context: `${COMPONENTS_PATH}/`,
-                from: `${COMPONENTS_PATH}/**/style/**/*.scss`,
-                to: `${DIST_PATH}/react/scss/components`,
-                transformPath: (targetPath) => targetPath.replace('/style/', '/'),
-            },
-        ]),
-    );
-    plugins.push(
-        new ReplaceInFileWebpackPlugin([
-            {
-                dir: `${DIST_PATH}/react/scss`,
-                rules: [
-                    {
-                        search: /'\.\/(base|elevation|grid|input|link|size|spacing|state|theme|typography)/g,
-                        replace: "'./core/$1",
-                    },
-                    {
-                        search: /'\.\.\/\.\.\/components/g,
-                        replace: "'./components",
-                    },
-                    {
-                        search: /\/style/g,
-                        replace: '',
-                    },
-                ],
-            },
-        ]),
-    );
-}
-
-if (minify) {
-    plugins.push(new HtmlMinifierPlugin(CONFIGS.htmlMinifier));
-    plugins.push(
-        new OptimizeCSSAssetsPlugin({
-            cssProcessorOptions: CONFIGS.cssNano,
-            cssProcessorPluginOptions: {},
-        }),
-    );
-    minimizer.push(
-        new TerserPlugin({
-            cache: true,
-            parallel: true,
-            sourceMap: true,
-            terserOptions: CONFIGS.terser,
+            title: 'LumX - React - Package',
         }),
     );
 }
@@ -132,7 +135,7 @@ module.exports = merge.smartStrategy({
     output: 'replace',
 })(reactConfig, {
     bail: true,
-    devtool: minify ? 'source-map' : '',
+    devtool: 'source-map',
     externals: {
         react: {
             amd: 'react',
@@ -148,7 +151,7 @@ module.exports = merge.smartStrategy({
         },
     },
     mode: 'production',
-    name: `react-umd${minify ? '-minified' : ''}`,
+    name: 'lumx-react-umd',
 
     module: {
         rules: getStyleLoader({ mode: 'prod' }),
@@ -165,14 +168,14 @@ module.exports = merge.smartStrategy({
         },
         libraryTarget: 'umd',
         path: distTechPath,
-        sourceMapFilename: minify ? `${filename}.js.map` : undefined,
+        sourceMapFilename: `${filename}.js.map`,
         umdNamedDefine: true,
     },
 
     plugins,
 
     optimization: {
-        minimize: minify,
+        minimize: true,
         minimizer,
     },
 });
