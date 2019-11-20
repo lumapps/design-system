8f4f17003d8c4473273e198b1c91bfd7245a7f96
pidupuis <pierre@lumapps.com>
2019-11-20 17:57:21 +0100

    refactor(archi): migrate avatar demo

---
+++ packages/site-demo/content/product/components/avatar/react/default.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { Avatar, ButtonEmphasis, IconButton, Size, Theme } from '@lumx/react';
+import { mdiPencil, mdiEye, mdiDelete } from '@lumx/icons';
+
+const App = ({ theme }) => (
+    <>
+        <Avatar theme={theme} image="http://i.pravatar.cc/40" size={Size.xs} />
+        <Avatar theme={theme} image="http://i.pravatar.cc/48" size={Size.s} />
+        <Avatar theme={theme} image="http://i.pravatar.cc/72" size={Size.m} />
+        <Avatar theme={theme} image="http://i.pravatar.cc/128" size={Size.l} />
+        <Avatar
+            theme={theme}
+            image="http://i.pravatar.cc/256"
+            size={Size.xl}
+            actions={
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <div className="lumx-spacing-margin-right-regular">
+                        <IconButton
+                            color="dark"
+                            emphasis={ButtonEmphasis.low}
+                            hasBackground={true}
+                            icon={mdiPencil}
+                            size={Size.s}
+                        />
+                    </div>
+
+                    <div className="lumx-spacing-margin-right-regular">
+                        <IconButton
+                            color="dark"
+                            emphasis={ButtonEmphasis.low}
+                            hasBackground={true}
+                            icon={mdiEye}
+                            size={Size.s}
+                        />
+                    </div>
+
+                    <div>
+                        <IconButton
+                            color="dark"
+                            emphasis={ButtonEmphasis.low}
+                            hasBackground={true}
+                            icon={mdiDelete}
+                            size={Size.s}
+                        />
+                    </div>
+                </div>
+            }
+        />
+    </>
+);
+
+export default App;
