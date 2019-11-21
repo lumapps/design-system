7b4b55676a0ccfc4a8720f45ab43b4022a3edc44
pidupuis <pierre@lumapps.com>
2019-11-21 15:30:11 +0100

    refactor(archi): migrate list demo

---
+++ packages/site-demo/content/product/components/list/react/regular.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {
+    Avatar,
+    Button,
+    ButtonEmphasis,
+    Icon,
+    IconButton,
+    List,
+    ListItem,
+    ListSubheader,
+    Size,
+    Thumbnail,
+    ThumbnailVariant,
+} from '@lumx/react';
+
+import { mdiDotsHorizontal, mdiSend } from '@lumx/icons';
+
+const App = () => (
+    <>
+        <List>
+            <ListSubheader>text only</ListSubheader>
+            <ListItem>Single-line item</ListItem>
+            <ListSubheader>rich</ListSubheader>
+            <ListItem before={<Icon icon={mdiSend} size={Size.xs} />}>Single-line item</ListItem>
+            <ListItem
+                before={<Thumbnail variant={ThumbnailVariant.rounded} image="https://picsum.photos/72" size={Size.m} />}
+                after={<IconButton emphasis={ButtonEmphasis.low} icon={mdiDotsHorizontal} />}
+            >
+                Single-line item
+            </ListItem>
+            <ListItem
+                before={<Avatar image="http://i.pravatar.cc/72" size={Size.m} />}
+                after={<Button emphasis={ButtonEmphasis.low}>Button</Button>}
+            >
+                Single-line item
+            </ListItem>
+        </List>
+    </>
+);
+
+export default App;
