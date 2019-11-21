6600c9c62e279c2f69aefd938eec770909d36d85
gcornut <guillaume.cornut@lumapps.com>
2019-11-21 11:27:20 +0100

    fix(archi): minor tsconfig/lint-staged fixes

--- configs/index.js
+++ configs/index.js
@@ -2,8 +2,7 @@ module.exports = {
     babel: require('./babel.config'),
     htmlMinifier: require('./html-minifier.config'),
     postcss: require('./postcss.config'),
-    tsconfig: require('./tsconfig.json'),
     cssNano: require('./css-nano.config'),
     terser: require('./terser.config'),
-    path: require('./path')
+    path: require('./path'),
 };
