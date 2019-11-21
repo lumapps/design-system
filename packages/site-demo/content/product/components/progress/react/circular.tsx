8a52a9efc98bad0d52f3dfb0e087d5253aa0c0d6
pidupuis <pierre@lumapps.com>
2019-11-21 15:38:18 +0100

    refactor(archi): migrate progress demo

---
+++ packages/site-demo/content/product/components/progress/react/circular.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Progress } from '@lumx/react';
+
+const App = ({ theme }) => (
+    <>
+        <Progress theme={theme} />
+    </>
+);
+
+export default App;
