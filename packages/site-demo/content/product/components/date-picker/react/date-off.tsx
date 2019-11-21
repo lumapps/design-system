c209eb2441754c5404e24966229e4a8e5d989891
pidupuis <pierre@lumapps.com>
2019-11-21 16:38:56 +0100

    refactor(archi): migrate date-picker demo

---
+++ packages/site-demo/content/product/components/date-picker/react/date-off.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+    <img width="336" src="datepicker-past-date-off.png" />
+);
+
+export default App;
---
+++ packages/site-demo/content/product/components/date-picker/react/date-on.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+    <img width="339" src="datepicker-past-date-on.png" />
+);
+
+export default App;
