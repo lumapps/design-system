8f4c31248416ad267c2e8308d63e5dc856dfc5c6
Juani Galan <juan@lumapps.com>
2019-11-27 09:38:18 +0100

    feat(addons): started adding several addons to storybook

--- packages/lumx-react/src/components/avatar/Avatar.stories.tsx
+++ packages/lumx-react/src/components/avatar/Avatar.stories.tsx
@@ -6,17 +6,21 @@ import '@lumx/core/lumx-theme-material.scss';
 import { mdiDelete, mdiEye, mdiPencil } from '@lumx/icons';
 import { Avatar, Emphasis, IconButton, Size } from '@lumx/react';
 
-export default { title: 'Avatar' };
+import { withA11y } from '@storybook/addon-a11y';
+import { text, withKnobs } from '@storybook/addon-knobs';
 
-export const xs = () => <Avatar image="http://i.pravatar.cc/40" size={Size.xs} />;
-export const s = () => <Avatar image="http://i.pravatar.cc/48" size={Size.s} />;
-export const m = () => <Avatar image="http://i.pravatar.cc/72" size={Size.m} />;
-export const l = () => <Avatar image="http://i.pravatar.cc/128" size={Size.l} />;
+export default { title: 'Avatar', decorators: [withKnobs, withA11y] };
+
+export const xs = () => <Avatar image={text('Image', 'http://i.pravatar.cc/40')} size={Size.xs} />;
+
+export const s = () => <Avatar image={text('Image', 'http://i.pravatar.cc/48')} size={Size.s} />;
+export const m = () => <Avatar image={text('Image', 'http://i.pravatar.cc/72')} size={Size.m} />;
+export const l = () => <Avatar image={text('Image', 'http://i.pravatar.cc/128')} size={Size.l} />;
 
 export const avatarWithActions = () => {
     return (
         <Avatar
-            image="http://i.pravatar.cc/256"
+            image={text('Image', 'http://i.pravatar.cc/256')}
             size={Size.xl}
             actions={
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
