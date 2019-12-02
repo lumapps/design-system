64f4753926c19421b1663f14c2cc21bffb0571f2
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-12-02 11:18:21 +0100

    chore(date-picker): update storybook demo

--- packages/lumx-react/src/components/date-picker/DatePicker.stories.tsx
+++ packages/lumx-react/src/components/date-picker/DatePicker.stories.tsx
@@ -6,4 +6,4 @@ import { decorators } from '@lumx/react/story-block';
 
 export default { title: 'DatePicker', decorators };
 
-export const simpleDatePicker = () => <DatePicker locale="fr" />;
+export const simpleDatePicker = () => <DatePicker locale="fr" minDate={new Date()} />;
