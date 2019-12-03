4dc07831cb1cc1c59042b4cb334f7c4937459f97
Vanessa <vanessa@lumapps.com>
2019-12-03 17:00:13 +0100

    docs(iconography): add images

---
+++ packages/site-demo/content/product/foundations/iconography/react/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+    <>
+        <img src="iconography/iconography-layout.png" width="128" />
+        <img src="iconography/iconography-keylines.png" width="128" />
+    </>
+);
+
+export default App;
