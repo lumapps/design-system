f3a165e45ea458a4294eb0b8a12ff76761a3310f
gcornut <guillaume.cornut@lumapps.com>
2019-12-06 15:23:06 +0100

    fix(demo): fix hot code reloading of SCSS/CSS styles

---
+++ packages/site-demo/src/theme-switch/index.ts
@@ -0,0 +1,25 @@
+import { Theme } from '@lumx/demo/context/theme';
+import { switchDevTheme } from '@lumx/demo/theme-switch/switch-dev';
+import { switchProdTheme } from '@lumx/demo/theme-switch/switch-prod';
+
+const getThemeId = (theme) => `theme-${theme}`;
+
+const getThemeElement = (theme: Theme) => document.getElementById(getThemeId(theme));
+
+async function switchToTheme(oldTheme: Theme, newTheme: Theme) {
+    const oldThemeElement = getThemeElement(oldTheme);
+
+    if (oldThemeElement?.tagName === 'LINK') {
+        // In production.
+        switchProdTheme(oldTheme, newTheme);
+        return;
+    }
+
+    if (oldThemeElement?.tagName === 'SCRIPT') {
+        // In development (for hot code reload).
+        switchDevTheme(oldTheme, newTheme);
+        return;
+    }
+}
+
+export { switchToTheme };
