78eb82d189c233aef6faec3aa72886fb02cf5e50
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-12-06 10:10:05 +0100

    chore: fix slideshow demo image block tags

--- packages/site-demo/content/product/components/slideshow/react/default.tsx
+++ packages/site-demo/content/product/components/slideshow/react/default.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-import { ImageBlock, ImageBlockCaptionPosition, Slideshow, SlideshowItem, ThumbnailAspectRatio } from '@lumx/react';
+import {
+    Alignment,
+    Chip,
+    ChipGroup,
+    ImageBlock,
+    ImageBlockCaptionPosition,
+    Size,
+    Slideshow,
+    SlideshowItem,
+    ThumbnailAspectRatio,
+} from '@lumx/react';
 
 const App = ({ theme }) => {
     const slideshowStyle = {
@@ -9,94 +19,121 @@ const App = ({ theme }) => {
 
     const imageBlockDemoProps = {
         description: 'What an image',
-        tags: ['#tag1', '#tag2', '#tag3'],
+        tags: (
+            <ChipGroup align={Alignment.left}>
+                <Chip size={Size.s} theme={theme}>
+                    Tag 1
+                </Chip>
+
+                <Chip size={Size.s} theme={theme}>
+                    Tag 2
+                </Chip>
+            </ChipGroup>
+        ),
         title: 'Nice Image',
     };
 
     return (
-        <Slideshow activeIndex={0} hasControls={true} theme={theme} autoPlay={false} groupBy={1} style={slideshowStyle}>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=31"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=1"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=2"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=8"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=3"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=4"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=5"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=36"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-            <SlideshowItem>
-                <ImageBlock
-                    aspectRatio={ThumbnailAspectRatio.horizontal}
-                    captionPosition={ImageBlockCaptionPosition.over}
-                    image="https://picsum.photos/640/480/?image=7"
-                    theme={theme}
-                    {...imageBlockDemoProps}
-                />
-            </SlideshowItem>
-        </Slideshow>
+        <div className="demo-grid">
+            <Slideshow
+                activeIndex={0}
+                hasControls={true}
+                theme={theme}
+                autoPlay={false}
+                groupBy={1}
+                style={slideshowStyle}
+            >
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=31"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=1"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=2"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=8"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=3"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=4"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=5"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=36"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+
+                <SlideshowItem>
+                    <ImageBlock
+                        aspectRatio={ThumbnailAspectRatio.horizontal}
+                        captionPosition={ImageBlockCaptionPosition.over}
+                        image="https://picsum.photos/640/480/?image=7"
+                        theme={theme}
+                        {...imageBlockDemoProps}
+                    />
+                </SlideshowItem>
+            </Slideshow>
+        </div>
     );
 };
 
