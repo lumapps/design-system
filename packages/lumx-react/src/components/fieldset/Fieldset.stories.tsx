c352f7e80c62ebf5d5b4f5edb252862690130409
Thomas V <thomas.vandermeersch@lumapps.com>
2019-12-23 15:06:16 +0100

    feat(fieldset): simplify component

--- packages/lumx-react/src/components/fieldset/Fieldset.stories.tsx
+++ packages/lumx-react/src/components/fieldset/Fieldset.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { select, text } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 
-import { FIELDSET_DEFAULT_PROPS, Fieldset, Size, TextField } from '@lumx/react';
+import { Fieldset, TextField } from '@lumx/react';
 
 import { decorators } from '@lumx/react/story-block';
 
@@ -13,15 +13,13 @@ const action = (val: string) => console.log(val);
 
 export const simpleFieldset = ({ theme }) => {
     const legendText = text('Legend', 'First block', 'Content');
-    const topSpacing = select('Top Spacing', Size, FIELDSET_DEFAULT_PROPS.topSpacing, 'Style');
-    const bottomSpacing = select('Bottom Spacing', Size, FIELDSET_DEFAULT_PROPS.bottomSpacing, 'Style');
 
     return (
         <>
-            <Fieldset legend={legendText} topSpacing={topSpacing} bottomSpacing={bottomSpacing} theme={theme}>
+            <Fieldset legend={legendText}>
                 <TextField label="Login" theme={theme} value="" onChange={action} />
             </Fieldset>
-            <Fieldset legend="Personal info" topSpacing={topSpacing} bottomSpacing={bottomSpacing} theme={theme}>
+            <Fieldset legend="Personal info" className="lumx-spacing-margin-top-huge">
                 <TextField label="First name" theme={theme} value="" onChange={action} />
                 <TextField label="Last name" theme={theme} value="" onChange={action} />
             </Fieldset>
--- packages/lumx-react/src/components/fieldset/Fieldset.tsx
+++ packages/lumx-react/src/components/fieldset/Fieldset.tsx
@@ -2,27 +2,15 @@ import React, { ReactElement } from 'react';
 
 import classNames from 'classnames';
 
-import { CSS_PREFIX } from '@lumx/core/js/constants';
-import { Size, Theme } from '@lumx/react';
 import { COMPONENT_PREFIX } from '@lumx/react/constants';
-import { IGenericProps, getRootClassName, handleBasicClasses } from '@lumx/react/utils';
+import { IGenericProps, getRootClassName } from '@lumx/react/utils';
 
 /**
  * Defines the props of the component.
  */
 interface IFieldsetProps extends IGenericProps {
     legend?: ReactElement | string;
-    topSpacing?: Size;
-    bottomSpacing?: Size;
-    theme?: Theme;
 }
-
-interface IDefaultPropsType extends Partial<IFieldsetProps> {}
-const FIELDSET_DEFAULT_PROPS: IDefaultPropsType = {
-    bottomSpacing: Size.big,
-    theme: Theme.light,
-    topSpacing: Size.huge,
-};
 
 const COMPONENT_NAME = `${COMPONENT_PREFIX}Fieldset`;
 
@@ -33,28 +21,16 @@ const COMPONENT_NAME = `${COMPONENT_PREFIX}Fieldset`;
 const CLASSNAME = getRootClassName(COMPONENT_NAME);
 
 const Fieldset: React.FC<IFieldsetProps> = ({
-    bottomSpacing = FIELDSET_DEFAULT_PROPS.bottomSpacing,
     className = '',
     children,
     legend,
-    theme = FIELDSET_DEFAULT_PROPS.theme,
-    topSpacing = FIELDSET_DEFAULT_PROPS.topSpacing,
     ...props
 }: IFieldsetProps): React.ReactElement => (
-    <fieldset className={classNames(className, CLASSNAME, handleBasicClasses({ prefix: CLASSNAME, theme }))} {...props}>
-        {legend && (
-            <legend
-                className={classNames(
-                    `${CSS_PREFIX}-spacing-margin-bottom-${bottomSpacing}`,
-                    `${CSS_PREFIX}-spacing-margin-top-${topSpacing}`,
-                )}
-            >
-                {legend}
-            </legend>
-        )}
+    <fieldset className={classNames(className, CLASSNAME)} {...props}>
+        {legend && <legend className={`${CLASSNAME}__legend`}>{legend}</legend>}
         {children}
     </fieldset>
 );
 Fieldset.displayName = COMPONENT_NAME;
 
-export { CLASSNAME, FIELDSET_DEFAULT_PROPS, Fieldset, IFieldsetProps };
+export { CLASSNAME, Fieldset, IFieldsetProps };
