6f4e931c5657edbdad7ed0e2dd524be138258104
Thomas V <thomas.vandermeersch@lumapps.com>
2019-12-23 16:14:35 +0100

    docs(popover): implement matching-width demo

--- packages/site-demo/content/product/components/popover/react/matching-width.tsx
+++ packages/site-demo/content/product/components/popover/react/matching-width.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
 
-import {} from '@lumx/react';
+import { mdiMagnify } from '@lumx/icons';
+import { Placement, Popover, TextField } from '@lumx/react';
 
-const App = () => (
-    <>
-        <img width="374" src="react/popover-matching-width.png" />
-    </>
-);
+const App = ({ theme }) => {
+    const [searchValue, setSearchValue] = React.useState('');
+    const anchorRef = React.useRef(null);
+    const popoverRef = React.useRef(null);
+
+    const { computedPosition, isVisible } = Popover.useComputePosition(
+        Placement.BOTTOM_START,
+        anchorRef,
+        popoverRef,
+        true,
+        undefined,
+        true,
+        true,
+    );
+
+    const demoPopoverHolderStyle = {
+        height: 132,
+    };
+
+    const demoPopperStyle = {
+        alignItems: 'center',
+        display: 'flex',
+        height: 80,
+        justifyContent: 'center',
+        width: computedPosition.width,
+    };
+
+    return (
+        <>
+            <div style={demoPopoverHolderStyle}>
+                <TextField
+                    value={searchValue}
+                    placeholder="Search"
+                    onChange={setSearchValue}
+                    icon={mdiMagnify}
+                    theme={theme}
+                    textFieldRef={anchorRef}
+                />
+                <Popover theme={theme} popoverRect={computedPosition} popoverRef={popoverRef} isVisible={isVisible}>
+                    <div style={demoPopperStyle}>Popover's width matching anchor's</div>
+                </Popover>
+            </div>
+        </>
+    );
+};
 
 export default App;
