bbf680f9f7cd3704ff415ea3f4022d6ffebddf66
Thomas V <thomas.vandermeersch@lumapps.com>
2019-12-23 16:13:51 +0100

    docs(popover): implement alignments demo

--- packages/site-demo/content/product/components/popover/react/alignments.tsx
+++ packages/site-demo/content/product/components/popover/react/alignments.tsx
@@ -1,11 +1,72 @@
 import React from 'react';
 
-import {} from '@lumx/react';
+import { Chip, Placement, Popover, Size } from '@lumx/react';
 
-const App = () => (
-    <>
-        <img width="741" src="react/popover-alignements.png" />
-    </>
-);
+const App = ({ theme }) => {
+    const demoWrapperStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+    };
+
+    const demoPopperStyle = {
+        alignItems: 'center',
+        display: 'flex',
+        height: 80,
+        justifyContent: 'center',
+        width: 350,
+    };
+
+    const demoPopoverHolderStyle = {
+        alignItems: 'end',
+        display: 'flex',
+        height: 132,
+        justifyContent: 'space-around',
+    };
+
+    const topStartAnchorRef = React.useRef(null);
+    const topStartPopoverRef = React.useRef(null);
+    const topStartPosition = Popover.useComputePosition(
+        Placement.TOP_START,
+        topStartAnchorRef,
+        topStartPopoverRef,
+        true,
+    );
+
+    const topEndAnchorRef = React.useRef(null);
+    const topEndPopoverRef = React.useRef(null);
+    const topEndPosition = Popover.useComputePosition(Placement.TOP_END, topEndAnchorRef, topEndPopoverRef, true);
+
+    return (
+        <div style={demoWrapperStyle}>
+            <div style={demoPopoverHolderStyle}>
+                <Chip chipRef={topStartAnchorRef} theme={theme} size={Size.s}>
+                    TOP_START
+                </Chip>
+            </div>
+            <Popover
+                theme={theme}
+                popoverRect={topStartPosition.computedPosition}
+                popoverRef={topStartPopoverRef}
+                isVisible={topStartPosition.isVisible}
+            >
+                <div style={demoPopperStyle}>{'Popover aligned on start of the top side'}</div>
+            </Popover>
+
+            <div style={demoPopoverHolderStyle}>
+                <Chip chipRef={topEndAnchorRef} theme={theme} size={Size.s}>
+                    TOP_END
+                </Chip>
+            </div>
+            <Popover
+                theme={theme}
+                popoverRect={topEndPosition.computedPosition}
+                popoverRef={topEndPopoverRef}
+                isVisible={topEndPosition.isVisible}
+            >
+                <div style={demoPopperStyle}>{'Popover aligned on start of the top side'}</div>
+            </Popover>
+        </div>
+    );
+};
 
 export default App;
