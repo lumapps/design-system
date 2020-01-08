88a75176034c5a876a387638f4ed150ae3cba8f5
Thomas V <thomas.vandermeersch@lumapps.com>
2020-01-08 14:35:42 +0100

    feat(textfield-helper): add the component to TextField

--- packages/lumx-react/src/components/text-field/TextField.stories.tsx
+++ packages/lumx-react/src/components/text-field/TextField.stories.tsx
@@ -1,6 +1,7 @@
-import { TextField } from '@lumx/react';
+import { TextField, TextFieldType } from '@lumx/react';
 import { decorators } from '@lumx/react/story-block';
 import { text } from '@storybook/addon-knobs';
+import noop from 'lodash/noop';
 import React from 'react';
 
 export default { title: 'TextField', decorators };
@@ -9,11 +10,41 @@ export default { title: 'TextField', decorators };
  * TextField story
  * @return simple TextField.
  */
-export const simpleTextField = () => (
+export const simpleTextField = ({ theme }) => (
     <TextField
-        value={''}
+        value={text('Value', 'myvalue')}
         label={text('Label', 'I am the label')}
         placeholder={text('Placeholder', 'ex: A value')}
-        type="text"
+        theme={theme}
+        type={TextFieldType.input}
+        onChange={noop}
+    />
+);
+
+export const textFieldWithHelp = ({ theme }) => (
+    <TextField
+        value={text('Value', 'myvalue')}
+        label={text('Label', 'I am the label')}
+        placeholder={text('Placeholder', 'ex: A value')}
+        theme={theme}
+        onChange={noop}
+        helper={<span>{text('Helper', 'ex: toto@acme.com')}</span>}
+    />
+);
+
+export const textFieldWithError = ({ theme }) => (
+    <TextField
+        value={text('Value', 'myvalue')}
+        label={text('Label', 'I am the label')}
+        placeholder={text('Placeholder', 'ex: A value')}
+        theme={theme}
+        onChange={noop}
+        helper={<span>{text('Helper', 'ex: toto@acme.com')}</span>}
+        hasError={true}
+        error={
+            <span>
+                You must provide <strong>something</strong>
+            </span>
+        }
     />
 );
