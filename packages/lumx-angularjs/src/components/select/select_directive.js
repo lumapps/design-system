e5fc88b232a75b1f9f8fa03a5c0c087cb81140aa
Matthias Manoukian <matthias.manoukian@gmail.com>
2020-01-09 10:26:17 +0100

    feat(select): compare id in priority to check if choice is in model

--- packages/lumx-angularjs/src/components/select/select_directive.js
+++ packages/lumx-angularjs/src/components/select/select_directive.js
@@ -121,6 +121,29 @@ function SelectController($document, $interpolate, $sce, $scope, $timeout, LxDro
     /////////////////////////////
 
     /**
+     * Check if two objects are equals according to their id in priority.
+     *
+     * @param  {Object} obj1 The first object to compare.
+     * @param  {Object} obj2 The second object to compare.
+     * @return {boolean} Whether obj1 is equal to obj2.
+     */
+    function _isEqual(obj1, obj2) {
+        if (angular.isUndefined(obj1) || angular.isUndefined(obj2)) {
+            return false;
+        }
+
+        if (angular.isDefined(obj1.id) && angular.isDefined(obj2.id)) {
+            return obj1.id === obj2.id;
+        } else if (angular.isDefined(obj1.uid) && angular.isDefined(obj2.uid)) {
+            return obj1.uid === obj2.uid;
+        } else if (angular.isDefined(obj1.uuid) && angular.isDefined(obj2.uuid)) {
+            return obj1.uuid === obj2.uuid;
+        }
+
+        return angular.equals(obj1, obj2);
+    }
+
+    /**
      * Returns the object index in an array.
      *
      * @param  {Array}  arr The array to check in.
@@ -129,7 +152,7 @@ function SelectController($document, $interpolate, $sce, $scope, $timeout, LxDro
      */
     function _arrayObjectIndexOf(arr, obj) {
         for (let i = 0, len = arr.length; i < len; i++) {
-            if (angular.equals(obj, arr[i])) {
+            if (_isEqual(obj, arr[i])) {
                 return i;
             }
         }
@@ -450,7 +473,7 @@ function SelectController($document, $interpolate, $sce, $scope, $timeout, LxDro
             return _arrayObjectIndexOf(lx.viewValue, choice) !== -1;
         }
 
-        return angular.equals(choice, lx.viewValue);
+        return _isEqual(choice, lx.viewValue);
     }
 
     /**
