dbdfb5a428fdd4297a39ae80af6cc3800ed09299
flodi <15898440+FlorentDinet@users.noreply.github.com>
2020-01-10 15:42:22 +0100

    docs(firter-and-sort): doc update

---
+++ packages/site-demo/content/product/patterns/filter-and sort/react/responsive-simple.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = ({}) => (
+    <>
+        <img src="assets/filter-and-sort-responsive-simple.png" width="100%" />
+    </>
+);
+
+export default App;
---
+++ packages/site-demo/content/product/patterns/filter-and sort/react/text.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = ({}) => (
+    <>
+        <img src="assets/filter-and-sort-text.png" width="100%" />
+    </>
+);
+
+export default App;
