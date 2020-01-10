de3978f6770a0bffcfd1086887a5fd0f2fe618ac
Thomas V <thomas.vandermeersch@lumapps.com>
2020-01-10 12:01:09 +0100

    feat(textfield-helper): update storybook to expose every case

--- packages/lumx-react/src/components/input-helper/InputHelper.stories.tsx
+++ packages/lumx-react/src/components/input-helper/InputHelper.stories.tsx
@@ -1,10 +1,22 @@
 import { InputHelper, Kind } from '@lumx/react';
 import { decorators } from '@lumx/react/story-block';
-import { select, text } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 import React from 'react';
 
 export default { title: 'Input Helper', decorators };
 
-export const simpleHelper = ({ theme }) => (
-    <InputHelper kind={select('Kind', Kind, Kind.info)} theme={theme} text={text('Helper Text', 'ex: toto@acme.com')} />
+export const informationHelper = ({ theme }) => (
+    <InputHelper kind={Kind.info} theme={theme} text={text('Text', 'ex: toto@acme.com')} />
+);
+
+export const errorHelper = ({ theme }) => (
+    <InputHelper kind={Kind.error} theme={theme} text={text('Text', 'You should be bold!')} />
+);
+
+export const validHelper = ({ theme }) => (
+    <InputHelper kind={Kind.valid} theme={theme} text={text('Text', 'Great, you are bold')} />
+);
+
+export const warningHelper = ({ theme }) => (
+    <InputHelper kind={Kind.warning} theme={theme} text={text('Text', 'This should be used carefuly')} />
 );
