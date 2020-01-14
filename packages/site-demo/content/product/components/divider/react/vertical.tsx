b03b4cba552a47e61bb3bfa947ac3a18ffadaa4a
Matthias Manoukian <matthias.manoukian@gmail.com>
2020-01-14 10:31:59 +0100

    chore: update divider  angularjs demo content

--- packages/site-demo/content/product/components/divider/react/vertical.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Divider } from '@lumx/react';
-
-const App = ({ theme }) => <Divider theme={theme} className="lumx-spacing-margin-vertical-big" />;
-
-export default App;
