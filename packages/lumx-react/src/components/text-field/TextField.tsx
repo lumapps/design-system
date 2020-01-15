8d6cd2c9d835ff3c4d81313290d24dde80d2cfbe
Matthias Manoukian <matthias.manoukian@gmail.com>
2020-01-15 10:20:28 +0100

    feat(input-helper): implement margin on each component

--- packages/lumx-react/src/components/text-field/TextField.tsx
+++ packages/lumx-react/src/components/text-field/TextField.tsx
@@ -406,11 +406,15 @@ const TextField: React.FC<TextFieldProps> = (props: TextFieldProps): ReactElemen
                 </div>
             </div>
             {hasError && error && (
-                <InputHelper kind={Kind.error} theme={theme}>
+                <InputHelper className={`${CLASSNAME}__helper`} kind={Kind.error} theme={theme}>
                     {error}
                 </InputHelper>
             )}
-            {helper && <InputHelper theme={theme}>{helper}</InputHelper>}
+            {helper && (
+                <InputHelper className={`${CLASSNAME}__helper`} theme={theme}>
+                    {helper}
+                </InputHelper>
+            )}
         </div>
     );
 };
