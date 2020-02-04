3d8f7035bcadb50e41b932e1545420f2b95f99ba
gcornut <guillaume.cornut@lumapps.com>
2020-02-04 16:27:33 +0100

    chore(button): add knobs on simple button story

--- packages/lumx-react/src/components/button/Button.stories.tsx
+++ packages/lumx-react/src/components/button/Button.stories.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { mdiSend } from '@lumx/icons';
 
-import { Button } from '@lumx/react';
+import { Button, ColorPalette, Emphasis, Size } from '@lumx/react';
+import { DEFAULT_PROPS } from '@lumx/react/components/button/Button';
+import { boolean, select, text } from '@storybook/addon-knobs';
+import React from 'react';
 
 export default { title: 'Button' };
 
-export const simpleButton = ({ theme }: any) => <Button theme={theme}>Simple button</Button>;
+export const simpleButton = ({ theme }: any) => {
+    return (
+        <Button
+            emphasis={select('Emphasis', Emphasis, DEFAULT_PROPS.emphasis)}
+            theme={theme}
+            rightIcon={select('Right icon', { none: undefined, mdiSend }, undefined)}
+            leftIcon={select('Left icon', { none: undefined, mdiSend }, undefined)}
+            size={select('Size', [Size.m, Size.s], DEFAULT_PROPS.size)}
+            isSelected={boolean('isSelected', Boolean(DEFAULT_PROPS.isSelected))}
+            color={select('color', ColorPalette, DEFAULT_PROPS.color)}
+            hasBackground={boolean('hasBackground', Boolean(DEFAULT_PROPS.hasBackground))}
+        >
+            {text('Button content', 'Simple button')}
+        </Button>
+    );
+};
