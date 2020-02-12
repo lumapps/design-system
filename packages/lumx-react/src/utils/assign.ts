b25c2fcfb823411ba3afdc89ca9b232bc98eec84
Flo5k5 <fnourrisse@gmail.com>
2020-02-12 15:29:40 +0100

    feat(focal-point-react): fix some types

--- packages/lumx-react/src/utils/assign.ts
+++ packages/lumx-react/src/utils/assign.ts
@@ -1,4 +1,4 @@
-export const assign = (target: any, ...sources) => {
+export const assign = (target: any, ...sources: any[]) => {
     sources.forEach((source) => Object.keys(source).forEach((key) => (target[key] = source[key])));
     return target;
 };
--- packages/lumx-react/src/utils/debounce.ts
+++ packages/lumx-react/src/utils/debounce.ts
@@ -1,6 +1,6 @@
-export function debounce(func: Function, debounceTime: number) {
+export function debounce(func: (...args: any) => any, debounceTime: number) {
     let timeout: any;
-    return function debouncedFunction(...args) {
+    return function debouncedFunction(...args: any[]) {
         clearTimeout(timeout);
         timeout = setTimeout(() => func(...args), debounceTime);
     };
