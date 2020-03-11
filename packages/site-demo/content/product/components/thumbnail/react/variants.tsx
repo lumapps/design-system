dba1a5fc0c02745df48bba58c1105f2f8f904a65
matmkian <matthias.manoukian@gmail.com>
2020-03-11 14:47:19 +0100

    fix: replace flexbox by grid in thumbnail demo

--- packages/site-demo/content/product/components/thumbnail/react/variants.tsx
+++ packages/site-demo/content/product/components/thumbnail/react/variants.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { Alignment, FlexBox, Orientation, Size, Thumbnail, ThumbnailVariant } from '@lumx/react';
+import { Alignment, Grid, Orientation, Size, Thumbnail, ThumbnailVariant } from '@lumx/react';
 
 const App = () => (
-    <FlexBox orientation={Orientation.horizontal} vAlign={Alignment.spaceAround}>
+    <Grid orientation={Orientation.horizontal} vAlign={Alignment.spaceAround}>
         <Thumbnail image="https://picsum.photos/150" size={Size.xl} variant={ThumbnailVariant.squared} />
         <Thumbnail image="https://picsum.photos/150" size={Size.xl} variant={ThumbnailVariant.rounded} />
-    </FlexBox>
+    </Grid>
 );
 
 export default App;
