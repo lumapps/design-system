6ef068f0ddb4cbd9531545ada8ad7794ac2fd026
Flo5k5 <fnourrisse@gmail.com>
2020-04-16 11:00:05 +0200

    feat(react-link): update story

--- packages/lumx-react/src/components/link/Link.stories.tsx
+++ packages/lumx-react/src/components/link/Link.stories.tsx
@@ -5,12 +5,28 @@ import React from 'react';
 export default { title: 'LumX components/Link' };
 
 export const simpleLink = () => (
-    <Link
-        href={text('Href', 'https://google.com')}
-        target={boolean('target blank', false) ? '_blank' : ''}
-        color={select('Color', ColorPalette, ColorPalette.blue)}
-        colorVariant={select('Color Variant', ColorVariant, ColorVariant.N)}
-    >
-        {text('Value', 'Here is a link')}
-    </Link>
+    <>
+        <Link
+            href={text('href', 'https://google.com', 'Link 1')}
+            target={boolean('target: _blank', false, 'Link 1') ? '_blank' : ''}
+            color={select('Color', ColorPalette, ColorPalette.blue, 'Link 1')}
+            colorVariant={select('Color Variant', ColorVariant, ColorVariant.N, 'Link 1')}
+        >
+            {text('Value', 'Here is a first link', 'Link 1')}
+        </Link>
+        <br />
+        <Link
+            href={text('href', 'https://google.fr', 'Link 2')}
+            target={boolean('target: _blank', false, 'Link 2') ? '_blank' : ''}
+        >
+            {text('Value', 'Here is a second link', 'Link 2')}
+        </Link>
+        <br />
+        <Link
+            href={text('href', 'https://google.co.jp', 'Link 3')}
+            target={boolean('target: _blank', false, 'Link 3') ? '_blank' : ''}
+        >
+            {text('Value', 'Here is a third link', 'Link 3')}
+        </Link>
+    </>
 );
