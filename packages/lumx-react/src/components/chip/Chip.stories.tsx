8795f35b61f5c6503f76b37fae1b8dd67e44f999
pidupuis <pierre@lumapps.com>
2020-05-11 14:15:00 +0200

    chore(storybook): move stories in good folder

--- packages/lumx-react/src/components/chip/Chip.stories.tsx
+++ packages/lumx-react/src/components/chip/Chip.stories.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import { Chip } from '@lumx/react';
 
-export default { title: 'Chip' };
+export default { title: 'LumX components/Chip' };
 
 export const simple = ({ theme }: any) => <Chip theme={theme}>The chip</Chip>;
