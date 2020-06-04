5a381b7e8f21f22e3c7b2b1a5d2a5f95be440169
Flo5k5 <fnourrisse@gmail.com>
2020-06-04 15:35:02 +0200

    fix(react-thumbnail): add default medium size for fallback icon

--- packages/lumx-react/src/components/thumbnail/Thumbnail.tsx
+++ packages/lumx-react/src/components/thumbnail/Thumbnail.tsx
@@ -193,7 +193,12 @@ const Thumbnail: React.FC<ThumbnailProps> = ({
         >
             {hasError &&
                 (typeof fallback === 'string' ? (
-                    <Icon className={`${CLASSNAME}__fallback`} icon={fallback as string} size={size} theme={theme} />
+                    <Icon
+                        className={`${CLASSNAME}__fallback`}
+                        icon={fallback as string}
+                        size={size || Size.m}
+                        theme={theme}
+                    />
                 ) : (
                     fallback
                 ))}
