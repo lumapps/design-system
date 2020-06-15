49a37303878aec810cc9fc6f1a32f6fe95669783
matmkian <matthias.manoukian@gmail.com>
2020-06-15 10:24:33 +0200

    fix(mosaic): fix layout on IE11

--- packages/lumx-react/src/components/mosaic/Mosaic.tsx
+++ packages/lumx-react/src/components/mosaic/Mosaic.tsx
@@ -41,6 +41,7 @@ const DEFAULT_PROPS: DefaultPropsType = {
 const Mosaic: React.FC<MosaicProps> = ({ className, theme = DEFAULT_PROPS.theme, thumbnails, ...props }) => (
     <div
         className={classNames(className, handleBasicClasses({ prefix: CLASSNAME, theme }), {
+            [`${CLASSNAME}--has-1-thumbnail`]: thumbnails?.length === 1,
             [`${CLASSNAME}--has-2-thumbnails`]: thumbnails?.length === 2,
             [`${CLASSNAME}--has-3-thumbnails`]: thumbnails?.length === 3,
             [`${CLASSNAME}--has-4-thumbnails`]: thumbnails?.length >= 4,
