c12ae5676cc413b3128a4bb125b92968c4964f24
gcornut <guillaume.cornut@lumapps.com>
2020-06-15 17:04:04 +0200

    fix(chip): fix click propagation on after/before

--- packages/lumx-react/src/components/chip/Chip.stories.tsx
+++ packages/lumx-react/src/components/chip/Chip.stories.tsx
@@ -1,7 +1,22 @@
+/* tslint:disable:jsx-no-lambda */
 import React from 'react';
 
-import { Chip } from '@lumx/react';
+import { mdiClose, mdiViewList } from '@lumx/icons';
+import { Chip, Icon } from '@lumx/react';
 
 export default { title: 'LumX components/Chip' };
 
 export const simple = ({ theme }: any) => <Chip theme={theme}>The chip</Chip>;
+
+export const withAfterAndBefore = ({ theme }: any) => (
+    <Chip
+        theme={theme}
+        after={<Icon icon={mdiClose} />}
+        onAfterClick={() => alert('clicked on close')}
+        before={<Icon icon={mdiViewList} />}
+        onClick={() => alert('clicked on chip')}
+        isClickable
+    >
+        content
+    </Chip>
+);
