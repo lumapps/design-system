0a9b5cddee172765c3e1d9d06a0cbbbf2b4e3c30
Thomas <thomas.vandermeersch@lumapps.com>
2020-06-24 15:39:16 +0200

    fix(lum-10327): fix datePicker display for 28 of february for ex

--- packages/lumx-react/src/components/date-picker/DatePickerField.stories.tsx
+++ packages/lumx-react/src/components/date-picker/DatePickerField.stories.tsx
@@ -68,3 +68,18 @@ export const customMonth = ({ theme }: any) => {
         />
     );
 };
+
+export const with28FebruarySelected = ({ theme }: any) => {
+    const [value, setValue] = React.useState<DatePickerProps['value']>(new Date('2019-02-28'));
+
+    return (
+        <DatePickerField
+            locale="fr"
+            label="Start date"
+            placeholder="Pick a date"
+            theme={theme}
+            onChange={setValue}
+            value={value}
+        />
+    );
+};
