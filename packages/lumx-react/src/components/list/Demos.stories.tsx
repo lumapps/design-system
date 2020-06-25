8b3c1ecdce3ece548884d962ab169f4575700648
gcornut <guillaume.cornut@lumapps.com>
2020-06-25 17:29:43 +0200

    chore(list): add list demos in story book

---
+++ packages/lumx-react/src/components/list/Demos.stories.tsx
@@ -0,0 +1,6 @@
+export default { title: 'LumX components/List/Demos' };
+
+export { default as tiny } from './demos/tiny';
+export { default as regular } from './demos/regular';
+export { default as big } from './demos/big';
+export { default as huge } from './demos/huge';
