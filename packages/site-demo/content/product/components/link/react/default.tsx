d802fe9c813e316d100dcd35de153d3906bf2d81
flodi <15898440+FlorentDinet@users.noreply.github.com>
2020-09-04 15:39:12 +0200

    docs(link): replaced demo images

--- packages/site-demo/content/product/components/link/react/default.tsx
+++ packages/site-demo/content/product/components/link/react/default.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import { mdiPencil } from '@lumx/icons';
+import { Alignment, FlexBox, Icon, Link, Orientation, Size } from '@lumx/react';
+import { boolean, text } from '@storybook/addon-knobs';
+
 const App = () => (
     <>
-        <img src="link/react/link-default.png" width="700" />
+        <Link href={text('href', '#', 'Link 1')} target={boolean('target: _blank', false, 'Link 1') ? '_blank' : ''}>
+            {text('Value', 'Default link', 'Link 1')}
+        </Link>
+        <br />
+        <br />
+        <Link href={text('href', '#', 'Link 2')} target={boolean('target: _blank', false, 'Link 2') ? '_blank' : ''}>
+            <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
+                <Icon icon={mdiPencil} size={Size.xs} className="lumx-spacing-margin-right-tiny" />
+                {text('Value', 'Link with an icon', 'Link 2')}
+            </FlexBox>
+        </Link>
+        <br />
+        <br />
+        <Link href={text('href', '#', 'Link 3')} target={boolean('target: _blank', false, 'Link 3') ? '_blank' : ''}>
+            <span className="lumx-typography-title">{text('Value', 'Link with Title typography', 'Link 3')}</span>
+        </Link>
     </>
 );
 
--- packages/site-demo/content/product/components/link/react/implicit.tsx
+++ packages/site-demo/content/product/components/link/react/implicit.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
 
+import { mdiPencil } from '@lumx/icons';
+import { Alignment, ColorPalette, ColorVariant, FlexBox, Icon, Link, Orientation, Size } from '@lumx/react';
+import { boolean, select, text } from '@storybook/addon-knobs';
+
 const App = () => (
     <>
-        <img src="link/react/link-implicit.png" width="700" />
+        <Link
+            href={text('href', '#', 'Link 4')}
+            target={boolean('target: _blank', false, 'Link 4') ? '_blank' : ''}
+            color={select('Color', ColorPalette, ColorPalette.dark, 'Link 4')}
+            colorVariant={select('Color Variant', ColorVariant, ColorVariant.N, 'Link 4')}
+        >
+            {text('Value', 'Default link', 'Link 4')}
+        </Link>
+        <br />
+        <br />
+        <Link
+            href={text('href', '#', 'Link 5')}
+            target={boolean('target: _blank', false, 'Link 5') ? '_blank' : ''}
+            color={select('Color', ColorPalette, ColorPalette.dark, 'Link 5')}
+            colorVariant={select('Color Variant', ColorVariant, ColorVariant.N, 'Link 5')}
+        >
+            <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
+                <Icon icon={mdiPencil} size={Size.xs} className="lumx-spacing-margin-right-tiny" />
+                {text('Value', 'Link with an icon', 'Link 5')}
+            </FlexBox>
+        </Link>
+        <br />
+        <br />
+        <Link
+            href={text('href', '#', 'Link 6')}
+            target={boolean('target: _blank', false, 'Link 6') ? '_blank' : ''}
+            color={select('Color', ColorPalette, ColorPalette.dark, 'Link 6')}
+            colorVariant={select('Color Variant', ColorVariant, ColorVariant.N, 'Link 6')}
+        >
+            <span className="lumx-typography-title">{text('Value', 'Link with Title typography', 'Link 6')}</span>
+        </Link>
     </>
 );
 
