2d6fa4d55428258cef896d1bc00987c68195710b
matmkian <matthias.manoukian@gmail.com>
2020-10-05 15:32:56 +0200

    docs(chip): refine documentation

--- packages/site-demo/content/product/components/chip/react/group.tsx
+++ packages/site-demo/content/product/components/chip/react/group.tsx
@@ -4,49 +4,47 @@ import { Chip, ChipGroup, Size } from '@lumx/react';
 
 const App = ({ theme }: any) => {
     return (
-        <div className="demo-grid">
-            <ChipGroup>
-                <Chip theme={theme} size={Size.s}>
-                    Apricot
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Apple
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Banana
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Blueberry
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Lemon
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Orange
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Peach
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Pear
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Pineapple
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Melon
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Raspberry
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Strawberry
-                </Chip>
-                <Chip theme={theme} size={Size.s}>
-                    Watermelon
-                </Chip>
-            </ChipGroup>
-        </div>
+        <ChipGroup>
+            <Chip theme={theme} size={Size.s}>
+                Apricot
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Apple
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Banana
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Blueberry
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Lemon
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Orange
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Peach
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Pear
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Pineapple
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Melon
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Raspberry
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Strawberry
+            </Chip>
+            <Chip theme={theme} size={Size.s}>
+                Watermelon
+            </Chip>
+        </ChipGroup>
     );
 };
 
