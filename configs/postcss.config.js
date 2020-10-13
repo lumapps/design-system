343b89d358a34095d3d831c51531fa93d8f1d0b5
pidupuis <pierre@lumapps.com>
2020-10-13 11:03:04 +0200

    chore(polyfill): fix flexbox gap on safari

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -1,9 +1,10 @@
-/* eslint-disable global-require, import/no-commonjs, import/unambiguous */
+/* eslint-disable global-require, import/no-commonjs, import/unambiguous, import/no-extraneous-dependencies */
 module.exports = {
     plugins: [
         require('postcss-flexbox-unboxer')(),
         require('autoprefixer')({
             grid: true,
         }),
+        require('flex-gap-polyfill')(),
     ],
 };
