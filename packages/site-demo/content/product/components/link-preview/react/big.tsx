8cf9faec97e8951cec471dce00a96a8545f74432
matmkian <matthias.manoukian@gmail.com>
2020-10-19 10:23:34 +0200

    docs(link-preview): add flag and enhance big demo

--- packages/site-demo/content/product/components/link-preview/react/big.tsx
+++ packages/site-demo/content/product/components/link-preview/react/big.tsx
@@ -4,14 +4,16 @@ import { LinkPreview, Size } from '@lumx/react';
 
 const App = ({ theme }: any) => {
     return (
-        <LinkPreview
-            title={'Link title'}
-            description={'Description'}
-            url={'https://google.com'}
-            size={Size.big}
-            theme={theme}
-            thumbnail={'https://picsum.photos/320/240'}
-        />
+        <div style={{ width: 350, margin: '0 auto' }}>
+            <LinkPreview
+                title={'Link title'}
+                description={'Description'}
+                url={'https://google.com'}
+                size={Size.big}
+                theme={theme}
+                thumbnail={'https://picsum.photos/320/240'}
+            />
+        </div>
     );
 };
 
