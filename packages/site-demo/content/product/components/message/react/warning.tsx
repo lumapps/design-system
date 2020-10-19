cfc929283509c50d14c209307821d3fde1e90047
matmkian <matthias.manoukian@gmail.com>
2020-10-19 15:17:48 +0200

    docs(message): enhance content

---
+++ packages/site-demo/content/product/components/message/react/warning.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Message, MessageKind } from '@lumx/react';
+
+const App = () => (
+    <Message kind={MessageKind.warning} hasBackground>
+        <p>
+            Message text quisque tincidunt lobortis dui non auctor. Donec porta, ligula volutpat vehicula aliquet, dui
+            sapien tempus felis, sed cursus diam ante.
+        </p>
+    </Message>
+);
+
+export default App;
