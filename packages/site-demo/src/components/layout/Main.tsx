534573f51c585b40f7d90053f2abfe33eaa530d5
gcornut <guillaume.cornut@lumapps.com>
2020-10-23 16:36:51 +0200

    chore(site-demo): fix incorrect main header markup

--- packages/site-demo/src/components/layout/Main.tsx
+++ packages/site-demo/src/components/layout/Main.tsx
@@ -1,14 +1,14 @@
 import { ErrorBoundary } from '@lumx/demo/components/ErrorBoundary';
+import { GlobalThemeSelector } from '@lumx/demo/components/GlobalThemeSelector';
+import { LumxVersion } from '@lumx/demo/components/LumxVersion';
 import { Alignment, FlexBox, Orientation } from '@lumx/react';
 import { Match } from '@reach/router';
 import React from 'react';
-import { GlobalThemeSelector } from '@lumx/demo/components/GlobalThemeSelector';
-import { LumxVersion } from '@lumx/demo/components/LumxVersion';
 
 export const Main: React.FC = ({ children }) => (
     <main className="main">
         <div className="main__wrapper">
-            <header className="main-header">
+            <div className="main-header">
                 <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
                     <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
                         <span className="lumx-typography-overline lumx-spacing-margin-right-regular">Theme</span>
@@ -25,7 +25,7 @@ export const Main: React.FC = ({ children }) => (
                         }
                     </Match>
                 </FlexBox>
-            </header>
+            </div>
             <div className="main-content">
                 <div className="main-content__wrapper">
                     <ErrorBoundary>{children}</ErrorBoundary>
