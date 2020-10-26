eb19af4b28e2762bcddbb013351c103b830fd08b
matmkian <matthias.manoukian@gmail.com>
2020-10-26 09:29:47 +0100

    chore(site-demo): add spacing design tokens

--- packages/site-demo/src/components/design-tokens/index.ts
+++ packages/site-demo/src/components/design-tokens/index.ts
@@ -2,3 +2,4 @@ export * from './DesignTokenGroup';
 export * from './DesignToken';
 export * from './ColorVariantsDesignTokenGroup';
 export * from './SizesDesignTokenGroup';
+export * from './SpacingsDesignTokenGroup';
