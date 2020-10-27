dc769dab58c6efb4b40aeaa25e60e10e61cd6f33
gcornut <guillaume.cornut@lumapps.com>
2020-10-27 12:24:01 +0100

    chore(site-demo): update and fix MDX dependency resolution

--- packages/site-demo/untypped-modules.d.ts
+++ packages/site-demo/untypped-modules.d.ts
@@ -2,7 +2,5 @@
  * List untypped modules here to declare them as explicit any.
  */
 declare module 'prismjs';
-declare module 'react-angular';
-declare module 'mdx-utils';
 declare module '@mdx-js/react';
 declare module '*.svg';
