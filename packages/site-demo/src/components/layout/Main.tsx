48ee39150eff84f1e3667b2711d544174e20a8f5
matmkian <matthias.manoukian@gmail.com>
2020-11-05 09:20:48 +0100

    chore(demo-site): enhance lumx version positioning

--- packages/site-demo/src/components/layout/Main.tsx
+++ packages/site-demo/src/components/layout/Main.tsx
@@ -1,6 +1,5 @@
 import { ErrorBoundary } from '@lumx/demo/components/ErrorBoundary';
 import { LumxVersion } from '@lumx/demo/components/LumxVersion';
-import { Alignment, FlexBox, Orientation } from '@lumx/react';
 import { Match } from '@reach/router';
 import React from 'react';
 
@@ -8,17 +7,7 @@ export const Main: React.FC = ({ children }) => (
     <main className="main">
         <div className="main__wrapper">
             <div className="main-header">
-                <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
-                    <Match path="/product/components/*">
-                        {({ match }) =>
-                            match && (
-                                <FlexBox marginAuto={Alignment.left}>
-                                    <LumxVersion />
-                                </FlexBox>
-                            )
-                        }
-                    </Match>
-                </FlexBox>
+                <Match path="/product/components/*">{({ match }) => match && <LumxVersion />}</Match>
             </div>
             <div className="main-content">
                 <div className="main-content__wrapper">
