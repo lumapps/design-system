00f56cd743f56270754f40805dd7bcc21d0dca7b
flodi <15898440+FlorentDinet@users.noreply.github.com>
2020-11-06 10:01:56 +0100

    docs(sizes): added sizes documentation

---
+++ packages/site-demo/content/product/foundations/sizes/react/t-shirt-example.tsx
@@ -0,0 +1,22 @@
+import { Alignment, FlexBox, Size, UserBlock } from '@lumx/react';
+import React from 'react';
+
+export const App = () => (
+    <>
+        <UserBlock
+            name="Emmitt O. Lum"
+            fields={['Creative developer', 'Denpasar']}
+            avatar={{ image: '../../components/avatar/assets/persona.png' }}
+            size={Size.m}
+            className="lumx-spacing-margin-right-huge"
+        />
+        <FlexBox
+            hAlign={Alignment.center}
+            vAlign={Alignment.center}
+            className="lumx-color-background-dark-L6 lumx-color-font-dark-L3"
+            style={{ width: '36px', height: '36px' }}
+        >
+            m
+        </FlexBox>
+    </>
+);
