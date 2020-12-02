e059f8da04d51b5f05b66f3ef27e9c0d725bd574
gcornut <guillaume.cornut@lumapps.com>
2020-12-02 17:21:44 +0100

    docs(link): use new link icon prop in demo

--- packages/site-demo/content/product/components/link/react/sizes.tsx
+++ packages/site-demo/content/product/components/link/react/sizes.tsx
@@ -1,35 +1,26 @@
 import { mdiPencil } from '@lumx/icons';
-import { Alignment, ColorPalette, FlexBox, Icon, Link, Orientation, Size, Theme } from '@lumx/react';
+import { ColorPalette, Link, Theme, Typography } from '@lumx/react';
 import React from 'react';
 
-export const App = ({ theme }: any) => (
-    <>
-        <Link color={theme === Theme.light ? ColorPalette.primary : ColorPalette.light}>
-            <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
-                <Icon icon={mdiPencil} size={Size.s} className="lumx-spacing-margin-right-tiny" />
-                <span className="lumx-typography-title">Link with Title typography</span>
-            </FlexBox>
-        </Link>
+export const App = ({ theme }: any) => {
+    const color = theme === Theme.light ? ColorPalette.primary : ColorPalette.light;
+    return (
+        <>
+            <Link leftIcon={mdiPencil} typography={Typography.title} color={color}>
+                Link with Title typography
+            </Link>
 
-        <Link color={theme === Theme.light ? ColorPalette.primary : ColorPalette.light}>
-            <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
-                <Icon icon={mdiPencil} size={Size.s} className="lumx-spacing-margin-right-tiny" />
-                <span className="lumx-typography-subtitle2">Link with Subtitle 2 typography</span>
-            </FlexBox>
-        </Link>
+            <Link leftIcon={mdiPencil} typography={Typography.subtitle2} color={color}>
+                Link with Subtitle 2 typography
+            </Link>
 
-        <Link color={theme === Theme.light ? ColorPalette.primary : ColorPalette.light}>
-            <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
-                <Icon icon={mdiPencil} size={Size.xs} className="lumx-spacing-margin-right-tiny" />
-                <span className="lumx-typography-body1">Link with Body 1 typography</span>
-            </FlexBox>
-        </Link>
+            <Link leftIcon={mdiPencil} typography={Typography.body1} color={color}>
+                Link with Body 1 typography
+            </Link>
 
-        <Link color={theme === Theme.light ? ColorPalette.primary : ColorPalette.light}>
-            <FlexBox orientation={Orientation.horizontal} hAlign={Alignment.center}>
-                <Icon icon={mdiPencil} size={Size.xxs} className="lumx-spacing-margin-right-tiny" />
-                <span className="lumx-typography-caption">Link with Caption typography</span>
-            </FlexBox>
-        </Link>
-    </>
-);
+            <Link leftIcon={mdiPencil} typography={Typography.caption} color={color}>
+                Link with Caption typography
+            </Link>
+        </>
+    );
+};
