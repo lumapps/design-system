5ca4a09339bee982fec8efee496148f6587627b4
Thomas VANDERMEERSCH <thomas.vandermeersch@lumapps.com>
2020-12-29 17:02:27 +0100

    fix(a11y-chips): update color used
    LUM-13688

--- packages/lumx-react/src/components/chip/Chip.stories.tsx
+++ packages/lumx-react/src/components/chip/Chip.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { mdiClose, mdiViewList } from '@lumx/icons';
 import { Chip, Icon } from '@lumx/react';
+import { ColorPalette } from '..';
 
 export default { title: 'LumX components/chip/Chip' };
 
@@ -17,5 +18,37 @@ export const WithAfterAndBefore = ({ theme }: any) => (
         isClickable
     >
         content
+    </Chip>
+);
+
+export const WithColor = ({ theme }: any) => (
+    <>
+        <Chip theme={theme} color={ColorPalette.primary}>
+            My chip
+        </Chip>
+        <Chip theme={theme} color={ColorPalette.secondary}>
+            My chip
+        </Chip>
+        <Chip theme={theme} color={ColorPalette.blue}>
+            My chip
+        </Chip>
+        <Chip theme={theme} color={ColorPalette.red}>
+            My chip
+        </Chip>
+        <Chip theme={theme} color={ColorPalette.dark}>
+            My chip
+        </Chip>
+    </>
+);
+
+export const Highlighted = ({ theme }: any) => (
+    <Chip theme={theme} color={ColorPalette.primary} isHighlighted>
+        My chip
+    </Chip>
+);
+
+export const Disabled = ({ theme }: any) => (
+    <Chip theme={theme} color={ColorPalette.primary} isDisabled>
+        My chip
     </Chip>
 );
