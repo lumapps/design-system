e42048858bfe52c89ab3acd5dc3fbd292b07d6ca
gcornut <guillaume.cornut@lumapps.com>
2021-01-04 16:22:22 +0100

    chore(build): fix docker demo site build

--- packages/lumx-react/src/hooks/useOnResize.ts
+++ packages/lumx-react/src/hooks/useOnResize.ts
@@ -1,8 +1,9 @@
 import { Callback, Falsy } from '@lumx/react/utils';
 import { MutableRefObject, RefObject, useEffect, useRef } from 'react';
+import { WINDOW } from '@lumx/react/constants';
 import { ResizeObserver as Polyfill } from '@juggle/resize-observer';
 
-const ResizeObserver: typeof Polyfill = (window as any).ResizeObserver || Polyfill;
+const ResizeObserver: typeof Polyfill = (WINDOW as any)?.ResizeObserver || Polyfill;
 
 export function useOnResize(element: HTMLElement | Falsy, update: RefObject<Callback>): void {
     const observerRef = useRef(null) as MutableRefObject<Polyfill | null>;
