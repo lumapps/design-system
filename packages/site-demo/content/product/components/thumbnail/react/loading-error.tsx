7fab50dae41fc2d807b2e083b2ac65a82a024294
Florent <15898440+FlorentDinet@users.noreply.github.com>
2021-10-25 17:01:08 +0200

    docs(thumbnail-update): added ratio and states explanations

---
+++ packages/site-demo/content/product/components/thumbnail/react/loading-error.tsx
@@ -0,0 +1,25 @@
+import { AspectRatio, Size, Thumbnail, ThumbnailVariant } from '@lumx/react';
+import React from 'react';
+
+export const App = ({ theme }: any) => (
+    <>
+        <Thumbnail
+            isLoading
+            image="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAoMBgDTD2qgAAAAASUVORK5CYII="
+            alt="loading image"
+            aspectRatio={AspectRatio.square}
+            size={Size.xl}
+            variant={ThumbnailVariant.rounded}
+            theme={theme}
+        />
+
+        <Thumbnail
+            image="brokenurl.jpg"
+            alt="broken url image"
+            aspectRatio={AspectRatio.square}
+            size={Size.xl}
+            variant={ThumbnailVariant.rounded}
+            theme={theme}
+        />
+    </>
+);
