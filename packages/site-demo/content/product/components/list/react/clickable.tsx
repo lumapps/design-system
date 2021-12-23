14609b92ebaf56f58dcf302ea33624f22449d4c3
FlorentDinet <15898440+FlorentDinet@users.noreply.github.com>
2021-12-23 15:49:17 +0100

    docs(list): updated documentations

---
+++ packages/site-demo/content/product/components/list/react/clickable.tsx
@@ -0,0 +1,17 @@
+import { List, ListItem, ListSubheader, Size } from '@lumx/react';
+import React from 'react';
+
+export const App = () => (
+    <List>
+        <ListItem linkProps={{ href: '#' }}>Clickable list item</ListItem>
+        <ListItem linkProps={{ href: '#' }} isDisabled>
+            Disabled clickable list item
+        </ListItem>
+        <ListItem linkProps={{ href: '#' }} isHighlighted>
+            Highlighted clickable list item
+        </ListItem>
+        <ListItem linkProps={{ href: '#' }} isSelected>
+            Selected clickable list item
+        </ListItem>
+    </List>
+);
