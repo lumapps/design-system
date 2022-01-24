d5066b6802a28f6a57c37c6a081de0f622c1f9fa
gcornut <guillaume.cornut@lumapps.com>
2022-01-24 17:41:58 +0100

    chore(thumbnail): udpate auto generated demo story

--- packages/lumx-react/src/stories/generated/Thumbnail/Demos.stories.tsx
+++ packages/lumx-react/src/stories/generated/Thumbnail/Demos.stories.tsx
@@ -4,6 +4,7 @@
 export default { title: 'LumX components/thumbnail/Thumbnail Demos' };
 
 export { App as Combined } from './combined';
+export { App as LoadingError } from './loading-error';
 export { App as Ratios } from './ratios';
 export { App as Sizes } from './sizes';
 export { App as Variants } from './variants';
