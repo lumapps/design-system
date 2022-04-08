b0b1cb3a8117dc0ae4dc7ca35c6441cc12a36564
Juan-Lucas <juanlucasbaltimore@gmail.com>
2022-04-08 11:53:40 +0200

    chore(comment-block): change date position and add tooltip for date

--- packages/lumx-react/src/components/comment-block/CommentBlock.stories.tsx
+++ packages/lumx-react/src/components/comment-block/CommentBlock.stories.tsx
@@ -19,7 +19,8 @@ export const WithHeaderActions = ({ theme }: any) => (
         ]}
         theme={theme}
         avatarProps={{ image: avatarImageKnob(), alt: 'Avatar' }}
-        date="4 hours ago"
+        date="5 days"
+        fullDate="Monday, March 30, 2021 at 4:06 PM"
         name="Emmitt O. Lum"
         text="All the rumors have finally died down and many skeptics have tightened their lips, the iPod does support video format now on its fifth generation."
         headerActions={<IconButton label="Actions" icon={mdiDotsHorizontal} emphasis={Emphasis.low} size={Size.s} />}
