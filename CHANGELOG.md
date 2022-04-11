b685dac5a6fbd5897b65a0d06ca39956c4a760cd
miko <michael.court@lumapps.com>
2022-04-11 18:42:55 +0200

    chore(core): disable animations if prefers-reduced-motion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+-   @lumx/core: all CSS animations and transitions are now disabled on browsers with `prefers-reduced-motion` media feature enabled.
+
 ## [2.2.13][] - 2022-04-08
 
 ### Added
--- packages/lumx-react/src/stories/generated/Toolbar/Demos.stories.tsx
+++ packages/lumx-react/src/stories/generated/Toolbar/Demos.stories.tsx
@@ -6,4 +6,5 @@ export default { title: 'LumX components/toolbar/Toolbar Demos' };
 export { App as Back } from './back';
 export { App as Default } from './default';
 export { App as Dialog } from './dialog';
+export { App as FileWidget } from './file-widget';
 export { App as MediaPicker } from './media-picker';
