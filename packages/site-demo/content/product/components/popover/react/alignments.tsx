f78de5a8cc8a5bc3980339d217c8a3471f807bef
FlorentDinet <15898440+FlorentDinet@users.noreply.github.com>
2022-06-24 15:01:32 +0200

    docs(responsive): fix CI issues

--- packages/site-demo/content/product/components/popover/react/alignments.tsx
+++ packages/site-demo/content/product/components/popover/react/alignments.tsx
@@ -1,4 +1,4 @@
-import { Alignment, Chip, FlexBox, Orientation, Placement, Popover, Size } from '@lumx/react';
+import { Chip, FlexBox, Orientation, Placement, Popover, Size } from '@lumx/react';
 import React, { useRef } from 'react';
 
 export const App = ({ theme }: any) => {
@@ -8,21 +8,21 @@ export const App = ({ theme }: any) => {
     return (
         <>
             <FlexBox style={{ marginTop: 80, gap: 128 }} orientation={Orientation.vertical}>
-            <Chip ref={topStartAnchorRef} theme={theme} size={Size.s} style={{ placeSelf: "flex-start" }}>
-                TOP_START
-            </Chip>
-            <Popover
-                isOpen
-                className="lumx-spacing-padding-huge"
-                theme={theme}
-                anchorRef={topStartAnchorRef}
-                placement={Placement.TOP_START}
-            >
-                Popover aligned on start of the top side
-            </Popover>
-            <Chip ref={topEndAnchorRef} theme={theme} size={Size.s} style={{ placeSelf: "flex-end" }}>
-                TOP_END
-            </Chip>
+                <Chip ref={topStartAnchorRef} theme={theme} size={Size.s} style={{ placeSelf: "flex-start" }}>
+                    TOP_START
+                </Chip>
+                <Popover
+                    isOpen
+                    className="lumx-spacing-padding-huge"
+                    theme={theme}
+                    anchorRef={topStartAnchorRef}
+                    placement={Placement.TOP_START}
+                >
+                    Popover aligned on start of the top side
+                </Popover>
+                <Chip ref={topEndAnchorRef} theme={theme} size={Size.s} style={{ placeSelf: "flex-end" }}>
+                    TOP_END
+                </Chip>
             </FlexBox>
             <Popover
                 isOpen
