14c7572fc4af56d899d252722119fa5e780791ef
Hadrien Chevallier <hadrien.chevallier@lumapps.com>
2022-08-01 14:37:18 +0200

    fix(thumbnail): fix shift calculation when focusY is 0
    CMS-2503

--- packages/lumx-react/src/components/thumbnail/useFocusPointStyle.tsx
+++ packages/lumx-react/src/components/thumbnail/useFocusPointStyle.tsx
@@ -3,11 +3,24 @@ import { AspectRatio } from '@lumx/react/components';
 import { ThumbnailProps } from '@lumx/react/components/thumbnail/Thumbnail';
 
 // Calculate shift to center the focus point in the container.
-function shiftPosition(scale: number, focusPoint: number, imageSize: number, containerSize: number) {
+export function shiftPosition({
+    scale,
+    focusPoint,
+    imageSize,
+    containerSize,
+}: {
+    scale: number;
+    focusPoint: number;
+    imageSize: number;
+    containerSize: number;
+}) {
+    if (imageSize === containerSize) return 0;
     const scaledSize = imageSize / scale;
+
     const scaledFocusHeight = focusPoint * scaledSize;
     const startFocus = scaledFocusHeight - containerSize / 2;
     const shift = startFocus / (scaledSize - containerSize);
+
     return Math.floor(Math.max(Math.min(shift, 1), 0) * 100);
 }
 
@@ -73,14 +86,23 @@ export const useFocusPointStyle = (
 
         // Focus Y relative to the top (instead of the center)
         const focusPointFromTop = Math.abs((focusPoint?.y || 0) - 1) / 2;
-        const y = shiftPosition(scale, focusPointFromTop, imageSize.height, containerSize.height);
+        const y = shiftPosition({
+            scale,
+            focusPoint: focusPointFromTop,
+            imageSize: imageSize.height,
+            containerSize: containerSize.height,
+        });
 
         // Focus X relative to the left (instead of the center)
         const focusPointFromLeft = Math.abs((focusPoint?.x || 0) + 1) / 2;
-        const x = shiftPosition(scale, focusPointFromLeft, imageSize.width, containerSize.width);
+        const x = shiftPosition({
+            scale,
+            focusPoint: focusPointFromLeft,
+            imageSize: imageSize.width,
+            containerSize: containerSize.width,
+        });
 
         const objectPosition = `${x}% ${y}%`;
-
         // Update only if needed.
         setStyle((oldStyle) => (oldStyle.objectPosition === objectPosition ? oldStyle : { objectPosition }));
     }, [aspectRatio, containerSize, element, focusPoint?.x, focusPoint?.y, image, imageSize]);
