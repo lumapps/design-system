9072ab5df6c73156dbc3d11c19387f1798d9ff1b
miko <michael.court@lumapps.com>
2022-09-19 16:55:05 +0200

    chore(utils): create getFocusableElements focus utils

---
+++ packages/lumx-react/src/utils/focus/getFocusableElements.ts
@@ -0,0 +1,7 @@
+import { DISABLED_SELECTOR, TABBABLE_ELEMENTS_SELECTOR } from './constants';
+
+const isNotDisabled = (element: HTMLElement) => !element.matches(DISABLED_SELECTOR);
+
+export function getFocusableElements(element: HTMLElement): HTMLElement[] {
+    return Array.from(element.querySelectorAll<HTMLElement>(TABBABLE_ELEMENTS_SELECTOR)).filter(isNotDisabled);
+}
