a5f4bd5a24ddfcad257cec3558b16e3c4b8a233b
gcornut <guillaume.cornut@lumapps.com>
2022-10-27 13:36:07 +0200

    chore(storybook): add auto generated dropdown demo stories

--- packages/lumx-react/src/stories/generated/Dropdown/Demos.stories.tsx
+++ packages/lumx-react/src/stories/generated/Dropdown/Demos.stories.tsx
@@ -4,4 +4,5 @@
 export default { title: 'LumX components/dropdown/Dropdown Demos' };
 
 export { App as Default } from './default';
+export { App as Selects } from './selects';
 export { App as Target } from './target';
