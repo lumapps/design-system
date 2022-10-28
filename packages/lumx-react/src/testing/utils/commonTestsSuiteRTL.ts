c3b436475837f3d12fe5c40942851f50bc253391
gcornut <guillaume.cornut@lumapps.com>
2022-10-28 16:26:00 +0200

    chore(test): install @testing-library/react

---
+++ packages/lumx-react/src/testing/utils/commonTestsSuiteRTL.ts
@@ -0,0 +1,55 @@
+import isEmpty from 'lodash/isEmpty';
+
+import { GenericProps } from '@lumx/react/utils/type';
+import { queryByClassName } from '@lumx/react/testing/utils/queries';
+
+interface CommonSetup {
+    props: GenericProps;
+    container: HTMLElement;
+}
+
+interface Options<S extends CommonSetup> {
+    baseClassName: string;
+    forwardClassName?: keyof S;
+    forwardAttributes?: keyof S;
+}
+
+type SetupFunction<S extends CommonSetup> = (props?: GenericProps) => S;
+
+/**
+ * Common tests on components
+ * - Check base class name and class name forwarding
+ * - Check props forwarding
+ */
+export function commonTestsSuiteRTL<S extends CommonSetup>(setup: SetupFunction<S>, options: Options<S>): void {
+    if (isEmpty(options)) {
+        return;
+    }
+    const { baseClassName, forwardClassName, forwardAttributes } = options;
+    describe('Common tests suite', () => {
+        it('should render with base class name', () => {
+            const { container } = setup();
+            expect(queryByClassName(container, baseClassName)).toBeInTheDocument();
+        });
+
+        if (forwardClassName) {
+            it('should forward any CSS class', () => {
+                const modifiedProps = {
+                    className: 'component component--is-tested',
+                };
+                const wrappers = setup(modifiedProps);
+                expect(wrappers[forwardClassName]).toHaveClass(modifiedProps.className);
+            });
+        }
+
+        if (forwardAttributes) {
+            it('should forward any other prop', () => {
+                const modifiedProps = {
+                    winter: 'is coming',
+                };
+                const wrappers = setup(modifiedProps);
+                expect(wrappers[forwardAttributes]).toHaveAttribute('winter', modifiedProps.winter);
+            });
+        }
+    });
+}
--- packages/lumx-react/src/testing/utils/index.ts
+++ packages/lumx-react/src/testing/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './commonTestsSuite';
+export * from './commonTestsSuiteRTL';
 
 export * from './itShouldRenderStories';
