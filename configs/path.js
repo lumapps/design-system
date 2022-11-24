ca9484dfb6c4c44cfcd10637108ab28ab4f01719
gcornut <guillaume.cornut@lumapps.com>
2022-11-24 09:34:28 +0100

    chore(ci): add release note workflow

--- configs/path.js
+++ configs/path.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
+
 const path = require('path');
 
 const ROOT_PATH = path.resolve(__dirname, '../');
+const CHANGELOG_PATH = path.resolve(ROOT_PATH, 'CHANGELOG.md');
 
 module.exports = {
     ROOT_PATH,
+    CHANGELOG_PATH,
 };
