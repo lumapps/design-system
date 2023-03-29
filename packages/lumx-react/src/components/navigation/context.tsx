8752315a86ba49b084992e82de2742b7a3892c3c
gcornut <guillaume.cornut@lumapps.com>
2023-03-29 10:45:46 +0200

    fix(navigation): fix default theme value

--- packages/lumx-react/src/components/navigation/context.tsx
+++ packages/lumx-react/src/components/navigation/context.tsx
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 import { Orientation } from '..';
 
-export const NavigationContext = createContext<{ orientation: Orientation } | undefined>({
+export const NavigationContext = createContext<{ orientation?: Orientation } | undefined>({
     orientation: Orientation.vertical,
 });
