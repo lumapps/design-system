a595b4740ae73bcbb0d73b2b5d7992191d1cb782
gcornut <guillaume.cornut@lumapps.com>
2023-06-06 11:01:41 +0200

    chore(test): rewrite flag unit tests to react testing library

--- packages/lumx-react/src/components/flag/Flag.test.tsx
+++ packages/lumx-react/src/components/flag/Flag.test.tsx
@@ -1,11 +1,10 @@
-import { mount, shallow } from 'enzyme';
-import 'jest-enzyme';
-import React, { ReactElement } from 'react';
+import React from 'react';
 
 import { ColorPalette, Theme } from '@lumx/react';
 import { mdiAbTesting } from '@lumx/icons';
-import { commonTestsSuite, Wrapper } from '@lumx/react/testing/utils';
-import { getBasicClass } from '@lumx/react/utils/className';
+import { commonTestsSuiteRTL } from '@lumx/react/testing/utils';
+import { getByClassName, queryByClassName } from '@lumx/react/testing/utils/queries';
+import { render } from '@testing-library/react';
 
 import { Flag, FlagProps } from './Flag';
 
@@ -14,66 +13,42 @@ const CLASSNAME = Flag.className as string;
 /**
  * Mounts the component and returns common DOM elements / data needed in multiple tests further down.
  */
-const setup = (propOverrides: Partial<FlagProps> = {}, shallowRendering = true) => {
-    const props = {
-        label: 'default',
-        ...propOverrides,
-    };
-    const renderer: (el: ReactElement) => Wrapper = shallowRendering ? shallow : mount;
-    const wrapper = renderer(<Flag {...props} />);
+const setup = (propOverrides: Partial<FlagProps> = {}) => {
+    const props = { label: 'default', ...propOverrides };
 
-    return {
-        props,
-        iconEl: wrapper.find('Icon'),
-        wrapper,
-    };
+    render(<Flag {...props} />);
+    const flag = getByClassName(document.body, CLASSNAME);
+    const icon = queryByClassName(flag, `${CLASSNAME}__icon`);
+
+    return { props, flag, icon };
 };
 
 describe(`<${Flag.displayName} />`, () => {
-    describe('Conditions', () => {
-        it('should use the icon', () => {
-            const { iconEl } = setup({ icon: mdiAbTesting });
-
-            expect(iconEl).toBeDefined();
-            expect(iconEl.prop('icon')).toEqual(mdiAbTesting);
+    describe('Props', () => {
+        it('should render default', () => {
+            const { flag, icon } = setup();
+            expect(flag).toBeInTheDocument();
+            expect(flag).toHaveClass(CLASSNAME);
+            expect(flag).toHaveClass(`${CLASSNAME}--color-dark`);
+            expect(icon).not.toBeInTheDocument();
         });
 
-        it('should have correct default color', () => {
-            const { wrapper } = setup({});
-            expect(wrapper).toHaveClassName(
-                getBasicClass({
-                    prefix: CLASSNAME,
-                    type: 'color',
-                    value: ColorPalette.dark,
-                }),
-            );
+        it('should render icon', () => {
+            const { icon } = setup({ icon: mdiAbTesting });
+            expect(icon).toBeInTheDocument();
         });
 
-        it('should switch color with theme', () => {
-            const { wrapper } = setup({ theme: Theme.dark });
-            expect(wrapper).toHaveClassName(
-                getBasicClass({
-                    prefix: CLASSNAME,
-                    type: 'color',
-                    value: ColorPalette.light,
-                }),
-            );
+        it('should render dark theme', () => {
+            const { flag } = setup({ theme: Theme.dark });
+            expect(flag).toHaveClass(`${CLASSNAME}--color-light`);
         });
 
-        it('should use the color', () => {
-            const color = ColorPalette.green;
-            const { wrapper } = setup({ icon: mdiAbTesting, color });
-
-            expect(wrapper).toHaveClassName(
-                getBasicClass({
-                    prefix: CLASSNAME,
-                    type: 'color',
-                    value: color,
-                }),
-            );
+        it('should render custom color', () => {
+            const { flag } = setup({ color: ColorPalette.red });
+            expect(flag).toHaveClass(`${CLASSNAME}--color-red`);
         });
     });
 
     // Common tests suite.
-    commonTestsSuite(setup, { className: 'wrapper', prop: 'wrapper' }, { className: CLASSNAME });
+    commonTestsSuiteRTL(setup, { baseClassName: CLASSNAME, forwardClassName: 'flag', forwardAttributes: 'flag' });
 });
