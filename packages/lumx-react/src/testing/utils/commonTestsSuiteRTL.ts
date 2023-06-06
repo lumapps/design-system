3822d7d44c0edc6bc5b1bb1ecf4db709d53591ed
gcornut <guillaume.cornut@lumapps.com>
2023-06-06 14:29:16 +0200

    chore(test): add forward ref test to common test suite

--- packages/lumx-react/src/testing/utils/commonTestsSuiteRTL.ts
+++ packages/lumx-react/src/testing/utils/commonTestsSuiteRTL.ts
@@ -2,6 +2,7 @@ import isEmpty from 'lodash/isEmpty';
 
 import { GenericProps } from '@lumx/react/utils/type';
 import { queryByClassName } from '@lumx/react/testing/utils/queries';
+import React from 'react';
 
 interface CommonSetup {
     props: GenericProps;
@@ -11,6 +12,7 @@ interface Options<S extends CommonSetup> {
     baseClassName: string;
     forwardClassName?: keyof S;
     forwardAttributes?: keyof S;
+    forwardRef?: keyof S;
 }
 
 type SetupFunction<S extends CommonSetup> = (props?: GenericProps) => S;
@@ -24,7 +26,7 @@ export function commonTestsSuiteRTL<S extends CommonSetup>(setup: SetupFunction<
     if (isEmpty(options)) {
         return;
     }
-    const { baseClassName, forwardClassName, forwardAttributes } = options;
+    const { baseClassName, forwardClassName, forwardAttributes, forwardRef } = options;
     describe('Common tests suite', () => {
         it('should render with base class name', () => {
             setup();
@@ -48,6 +50,14 @@ export function commonTestsSuiteRTL<S extends CommonSetup>(setup: SetupFunction<
                 };
                 const wrappers = setup(modifiedProps);
                 expect(wrappers[forwardAttributes]).toHaveAttribute('winter', modifiedProps.winter);
+            });
+        }
+
+        if (forwardRef) {
+            it('should forward ref', () => {
+                const ref = React.createRef();
+                const wrappers = setup({ ref });
+                expect(ref.current).toBe(wrappers[forwardRef]);
             });
         }
     });
