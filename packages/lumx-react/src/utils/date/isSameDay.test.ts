9fa47d6281231002154cf8d8196f605535cc56e3
gcornut <guillaume.cornut@lumapps.com>
2023-10-02 11:34:28 +0200

    fix(date-picker): fix error on invalid date when calling isSameDay

--- packages/lumx-react/src/utils/date/isSameDay.test.ts
+++ packages/lumx-react/src/utils/date/isSameDay.test.ts
@@ -21,7 +21,17 @@ describe(isSameDay, () => {
 
     it('should handle different years', () => {
         const date1 = new Date('2022-12-25T18:30:00');
-        const date2 = new Date('2023-12-25T18:30:00');
+        const date2 = new Date('1923-12-25T18:30:00');
         expect(isSameDay(date1, date2)).toBe(false);
+    });
+
+    it('should handle invalid date', () => {
+        // Invalid date input are not comparable, so we always return `false`
+        // Undefined date
+        expect(isSameDay(undefined as any, undefined as any)).toBe(false);
+        // Null date
+        expect(isSameDay(null as any, new Date())).toBe(false);
+        // Invalid date
+        expect(isSameDay(new Date('-'), new Date('-'))).toBe(false);
     });
 });
--- packages/lumx-react/src/utils/date/isSameDay.ts
+++ packages/lumx-react/src/utils/date/isSameDay.ts
@@ -1,7 +1,11 @@
+import { isDateValid } from '@lumx/react/utils/date/isDateValid';
+
 /**
  * Check `date1` is on the same day as `date2`.
  */
 export const isSameDay = (date1: Date, date2: Date) =>
+    isDateValid(date1) &&
+    isDateValid(date2) &&
     date1.getFullYear() === date2.getFullYear() &&
     date1.getMonth() === date2.getMonth() &&
     date1.getDate() === date2.getDate();
