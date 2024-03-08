9bf502b382f3f7c200ec480f49124029ab2a332c
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-03-08 15:57:52 +0100

    chore(storybook): migrate to v8 with webpack and swc

--- packages/lumx-react/.storybook/main.ts
+++ packages/lumx-react/.storybook/main.ts
@@ -1,28 +1,51 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
 import generateDemoStories from './generate-demo-stories';
-import postcss from './postcss.config';
+import postcssOptions from './postcss.config';
 
 // Generate storybook stories from demo site demos.
 generateDemoStories();
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-webpack5-compiler-swc',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
+        '@storybook/addon-interactions',
+    ],
     staticDirs: ['../../site-demo/static/'],
     framework: {
-        name: '@storybook/react-vite',
+        name: '@storybook/react-webpack5',
         options: {},
     },
     docs: {
         autodocs: 'tag',
     },
-    async viteFinal(config) {
-        return mergeConfig(config, {
-            plugins: [tsconfigPaths()],
-            css: { postcss },
-        });
+    webpackFinal: async (config) => {
+        if (config.module?.rules) {
+            /** S?CSS Loader */
+            config.module.rules.push({
+                  test: /\.s?css$/,
+                  sideEffects: true,
+                  use: [
+                      'style-loader',
+                      'css-loader',
+                      { loader: 'postcss-loader', options: { postcssOptions } },
+                      'sass-loader',
+                  ],
+            });
+        }
+        if (config.resolve) {
+            config.resolve.plugins = [
+                ...(config.resolve.plugins || []),
+                new TsconfigPathsPlugin({
+                    extensions: config.resolve.extensions,
+                }),
+            ];
+        }
+        return config;
     },
 };
 export default config;
