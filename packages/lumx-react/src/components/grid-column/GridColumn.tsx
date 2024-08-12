56b2bd5583de227a2cd6f449067a851ea6b4f259
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-08-12 13:23:53 +0200

    refactor(utils): remove use of lodash/isInteger

--- packages/lumx-react/src/components/grid-column/GridColumn.tsx
+++ packages/lumx-react/src/components/grid-column/GridColumn.tsx
@@ -1,6 +1,5 @@
 import { ReactElement, ReactNode } from 'react';
 
-import isInteger from 'lodash/isInteger';
 import classNames from 'classnames';
 
 import { Size } from '@lumx/react';
@@ -69,7 +68,7 @@ export const GridColumn = forwardRef<GridColumnProps>((props, ref): ReactElement
             className={classNames(className, CLASSNAME)}
             style={{
                 ...style,
-                ['--lumx-grid-column-item-min-width' as any]: isInteger(itemMinWidth) && `${itemMinWidth}px`,
+                ['--lumx-grid-column-item-min-width' as any]: Number.isInteger(itemMinWidth) && `${itemMinWidth}px`,
                 ['--lumx-grid-column-columns' as any]: maxColumns,
                 ['--lumx-grid-column-gap' as any]: gap && `var(--lumx-spacing-unit-${gap})`,
             }}
