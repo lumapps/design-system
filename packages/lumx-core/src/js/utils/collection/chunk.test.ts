68cdb396197b61d0857278823365de117b3eedfb
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-08-12 14:57:15 +0200

    refactor(utils): remove use of lodash/chunk

---
+++ packages/lumx-core/src/js/utils/collection/chunk.test.ts
@@ -0,0 +1,15 @@
+import { chunk } from '@lumx/core/js/utils/collection/chunk';
+
+describe(chunk, () => {
+    it('should do nothing on empty array', () => {
+        expect(chunk([], 2)).toEqual([]);
+    });
+
+    it('should work with size larger than input array', () => {
+        expect(chunk([1, 2], 4)).toEqual([[1, 2]]);
+    });
+
+    it('should chunk array with size not perfectly dividing the array length', () => {
+        expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+    });
+});
