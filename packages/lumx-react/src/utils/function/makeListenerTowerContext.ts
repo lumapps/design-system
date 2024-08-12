060080c500773658fe8fc893cef53bb58c60a903
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-08-12 14:39:10 +0200

    refactor(utils): remove use of lodash/pull

--- packages/lumx-react/src/utils/function/makeListenerTowerContext.ts
+++ packages/lumx-react/src/utils/function/makeListenerTowerContext.ts
@@ -1,5 +1,4 @@
 import last from 'lodash/last';
-import pull from 'lodash/pull';
 
 export type Listener = { enable(): void; disable(): void };
 
@@ -24,7 +23,10 @@ export function makeListenerTowerContext() {
             // Disable current listener.
             listener.disable();
             // Remove current listener.
-            pull(LISTENERS, listener);
+            const index = LISTENERS.indexOf(listener);
+            if (index > -1) {
+                LISTENERS.splice(index, 1);
+            }
             // Enable previous listener.
             last(LISTENERS)?.enable();
         },
