13c144a034f18d31bb6a417c77332a1d537bddeb
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-08-12 13:22:06 +0200

    refactor(utils): remove use of lodash/noop

--- packages/lumx-react/src/components/select/Select.stories.tsx
+++ packages/lumx-react/src/components/select/Select.stories.tsx
@@ -1,7 +1,6 @@
 import { mdiBullhornOutline } from '@lumx/icons';
 import { List, ListItem, Select, Size, TextField } from '@lumx/react';
 import { useBooleanState } from '@lumx/react/hooks/useBooleanState';
-import noop from 'lodash/noop';
 import range from 'lodash/range';
 import { SyntheticEvent, useState } from 'react';
 import { SelectVariant } from './constants';
@@ -9,6 +8,10 @@ import { SelectVariant } from './constants';
 export default {
     title: 'LumX components/select/Select',
     component: Select,
+    argTypes: {
+        onInputClick: { action: true },
+        onDropdownClose: { action: true },
+    },
 };
 
 const CHOICES = ['First item', 'Second item', 'Third item'];
@@ -114,8 +117,8 @@ export const DisabledSelect = () => {
             value=""
             label="My select"
             placeholder="Placeholder"
-            onInputClick={noop}
-            onDropdownClose={noop}
+            onInputClick={onInputClick}
+            onDropdownClose={onDropdownClose}
             isDisabled
         >
             <List isClickable>
@@ -202,7 +205,13 @@ export const SelectWithAnotherField = () => {
 
     return (
         <>
-            <TextField value="myvalue" label="I am the label" placeholder="ex: A value" onChange={noop} />
+            <TextField
+                value="myvalue"
+                label="I am the label"
+                placeholder="ex: A value"
+
+                onChange={() => {}}
+            />
             <Select
                 style={{ width: '100%' }}
                 isOpen={isOpen}
