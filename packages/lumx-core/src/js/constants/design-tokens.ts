dc9032f35707d2ded0c240fb94a13993570718a3
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-11-29 15:01:15 +0100

    feat(chip): rework selected state style for better a11y

--- packages/lumx-core/src/js/constants/design-tokens.ts
+++ packages/lumx-core/src/js/constants/design-tokens.ts
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Thu, 09 Jan 2025 14:20:03 GMT
+ * Generated on Thu, 09 Jan 2025 16:42:12 GMT
  */
 
 export const DESIGN_TOKENS = {
@@ -377,9 +377,40 @@ export const DESIGN_TOKENS = {
     chip: {
         'emphasis-selected': {
             'state-default': {
-                'border-width': { value: '0' },
+                'border-width': { value: '1px' },
                 'theme-light': {
-                    'border-color': { value: 'transparent' },
+                    'border-color': {
+                        value: 'var(--lumx-color-primary-N)',
+                        attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 1 } },
+                        $aliasedFrom: 'color.primary.N',
+                    },
+                    'background-color': {
+                        value: 'var(--lumx-color-primary-L6)',
+                        attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 0.05 } },
+                        $aliasedFrom: 'color.primary.L6',
+                    },
+                },
+                'theme-dark': {
+                    'border-color': {
+                        value: 'var(--lumx-color-light-N)',
+                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 1 } },
+                        $aliasedFrom: 'color.light.N',
+                    },
+                    'background-color': {
+                        value: 'var(--lumx-color-light-L6)',
+                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 0.1 } },
+                        $aliasedFrom: 'color.light.L6',
+                    },
+                },
+            },
+            'state-hover': {
+                'border-width': { value: '1px' },
+                'theme-light': {
+                    'border-color': {
+                        value: 'var(--lumx-color-primary-N)',
+                        attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 1 } },
+                        $aliasedFrom: 'color.primary.N',
+                    },
                     'background-color': {
                         value: 'var(--lumx-color-primary-L5)',
                         attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 0.1 } },
@@ -387,18 +418,26 @@ export const DESIGN_TOKENS = {
                     },
                 },
                 'theme-dark': {
-                    'border-color': { value: 'transparent' },
+                    'border-color': {
+                        value: 'var(--lumx-color-light-N)',
+                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 1 } },
+                        $aliasedFrom: 'color.light.N',
+                    },
                     'background-color': {
-                        value: 'var(--lumx-color-light-L3)',
-                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 0.6 } },
-                        $aliasedFrom: 'color.light.L3',
+                        value: 'var(--lumx-color-light-L5)',
+                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 0.2 } },
+                        $aliasedFrom: 'color.light.L5',
                     },
                 },
             },
-            'state-hover': {
-                'border-width': { value: '0' },
+            'state-active': {
+                'border-width': { value: '1px' },
                 'theme-light': {
-                    'border-color': { value: 'transparent' },
+                    'border-color': {
+                        value: 'var(--lumx-color-primary-N)',
+                        attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 1 } },
+                        $aliasedFrom: 'color.primary.N',
+                    },
                     'background-color': {
                         value: 'var(--lumx-color-primary-L4)',
                         attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 0.2 } },
@@ -406,30 +445,15 @@ export const DESIGN_TOKENS = {
                     },
                 },
                 'theme-dark': {
-                    'border-color': { value: 'transparent' },
+                    'border-color': {
+                        value: 'var(--lumx-color-light-N)',
+                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 1 } },
+                        $aliasedFrom: 'color.light.N',
+                    },
                     'background-color': {
                         value: 'var(--lumx-color-light-L4)',
                         attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 0.4 } },
                         $aliasedFrom: 'color.light.L4',
-                    },
-                },
-            },
-            'state-active': {
-                'border-width': { value: '0' },
-                'theme-light': {
-                    'border-color': { value: 'transparent' },
-                    'background-color': {
-                        value: 'var(--lumx-color-primary-L3)',
-                        attributes: { hex: '245be7', rgb: { r: 36, g: 91, b: 231, a: 0.4 } },
-                        $aliasedFrom: 'color.primary.L3',
-                    },
-                },
-                'theme-dark': {
-                    'border-color': { value: 'transparent' },
-                    'background-color': {
-                        value: 'var(--lumx-color-light-L5)',
-                        attributes: { hex: 'ffffff', rgb: { r: 255, g: 255, b: 255, a: 0.2 } },
-                        $aliasedFrom: 'color.light.L5',
                     },
                 },
             },
