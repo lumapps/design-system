a98f943aa7009430c6366d3d2eec4026a868ce62
Guillaume Cornut <guillaume.cornut@lumapps.com>
2025-01-30 13:29:14 +0100

    refactor(utils): remove use of lodash/throttle

--- packages/lumx-react/src/hooks/useSizeOnWindowResize.ts
+++ packages/lumx-react/src/hooks/useSizeOnWindowResize.ts
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import throttle from 'lodash/throttle';
-import { RectSize } from '@lumx/react/utils/type';
+import type { RectSize } from '@lumx/react/utils/type';
 
 /**
  * Observe element size (only works if it's size depends on the window size).
@@ -13,14 +12,19 @@ import { RectSize } from '@lumx/react/utils/type';
  */
 export function useSizeOnWindowResize(elementRef: React.RefObject<HTMLElement>): [RectSize | null, () => void] {
     const [size, setSize] = React.useState<null | RectSize>(null);
-    const updateSize = React.useMemo(
-        () =>
-            throttle(() => {
-                const newSize = elementRef.current?.getBoundingClientRect();
-                if (newSize) setSize(newSize);
-            }, 10),
-        [elementRef],
-    );
+    const updateSize = React.useMemo(() => {
+        let prevTimeout: ReturnType<typeof setTimeout> | undefined;
+        function update() {
+            const newSize = elementRef.current?.getBoundingClientRect();
+            if (newSize) setSize(newSize);
+            prevTimeout = undefined;
+        }
+        return () => {
+            if (!prevTimeout) {
+                prevTimeout = setTimeout(update, 10);
+            }
+        };
+    }, [elementRef]);
     React.useEffect(() => {
         updateSize();
         window.addEventListener('resize', updateSize);
