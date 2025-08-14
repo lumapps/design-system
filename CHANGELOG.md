fdba4959a34dfa516c922f9832ad6148e0f93dd0
Guillaume Cornut <gcornut@users.noreply.github.com>
2025-08-14 16:28:36 +0200

    chore(lumx-angularjs): migrate to webpack 5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 -   `@lumx/core`: rework internal build
+-   `@lumx/angularjs`: rework internal build
 
 ## [3.15.1][] - 2025-08-13
 
--- packages/lumx-angularjs/webpack.config.js
+++ packages/lumx-angularjs/webpack.config.js
@@ -1,20 +1,10 @@
-/* eslint-disable import/no-nodejs-modules */
-/* eslint-disable import/no-commonjs */
-/* eslint-disable import/unambiguous */
-/* eslint-disable import/no-extraneous-dependencies */
-
-const IS_CI = require('is-ci');
 const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const HtmlMinifierPlugin = require('html-minifier-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
-const WebpackBar = require('webpackbar');
-const WebpackNotifierPlugin = require('webpack-notifier');
 
 const CONFIGS = require('../../configs');
 
@@ -24,52 +14,6 @@ const SRC_PATH = `${PKG_PATH}/src`;
 const DIST_PATH = `${PKG_PATH}/dist`;
 
 const filename = '[name].min';
-
-const minimizer = [
-    new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-        terserOptions: CONFIGS.terser,
-    }),
-];
-
-const plugins = [
-    /* Clean output. */
-    new CleanWebpackPlugin(),
-
-    new WebpackBar(),
-    new FriendlyErrorsWebpackPlugin(),
-    new UnminifiedWebpackPlugin(),
-    new CopyWebpackPlugin([
-        {
-            from: `${CONFIGS.path.ROOT_PATH}/CONTRIBUTING.md`,
-            to: DIST_PATH,
-        },
-        {
-            from: `${CONFIGS.path.ROOT_PATH}/LICENSE.md`,
-            to: DIST_PATH,
-        },
-        {
-            from: `${PKG_PATH}/README.md`,
-            to: DIST_PATH,
-        },
-        {
-            from: `${PKG_PATH}/package.json`,
-            to: DIST_PATH,
-        },
-    ]),
-    new HtmlMinifierPlugin(CONFIGS.htmlMinifier),
-];
-
-if (!IS_CI) {
-    plugins.push(
-        new WebpackNotifierPlugin({
-            alwaysNotify: true,
-            title: `LumX - ${PKG_NAME} Package`,
-        }),
-    );
-}
 
 module.exports = {
     entry: {
@@ -119,23 +63,44 @@ module.exports = {
         filename: `${filename}.js`,
         path: DIST_PATH,
         library: {
-            root: 'LumX',
-            amd: PKG_NAME,
-            commonjs: PKG_NAME,
+            name: {
+                root: 'LumX',
+                amd: PKG_NAME,
+                commonjs: PKG_NAME,
+            },
+            type: 'umd',
+            umdNamedDefine: true,
         },
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
     },
 
-    plugins,
+    plugins: [
+        new CleanWebpackPlugin(),
+        new UnminifiedWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: `${CONFIGS.path.ROOT_PATH}/CONTRIBUTING.md`,
+                    to: DIST_PATH,
+                },
+                {
+                    from: `${CONFIGS.path.ROOT_PATH}/LICENSE.md`,
+                    to: DIST_PATH,
+                },
+                {
+                    from: `${PKG_PATH}/README.md`,
+                    to: DIST_PATH,
+                },
+                {
+                    from: `${PKG_PATH}/package.json`,
+                    to: DIST_PATH,
+                },
+            ],
+        }),
+    ],
 
     optimization: {
         minimize: true,
-        minimizer,
-    },
-
-    stats: {
-        colors: !IS_CI,
+        minimizer: [new TerserPlugin()],
     },
 
     performance: {
