8275360b5fc0033b2e37b9aa1a4a21ab12897c65
Guillaume Cornut <gcornut@users.noreply.github.com>
2025-09-11 13:36:49 +0200

    fix(notification): remove unwanted code string in render

--- packages/lumx-react/src/components/notification/Notification.tsx
+++ packages/lumx-react/src/components/notification/Notification.tsx
@@ -130,7 +130,6 @@ export const Notification = forwardRef<NotificationProps, HTMLDivElement>((props
                     </div>
                 )}
             </div>
-            );
         </Portal>
     );
 });
