96d227dc4229b9a3d5fd70879336e0683e4a480c
Juani Galán <juan@lumapps.com>
2025-11-18 09:10:06 +0100

    chore(core): create Icon JSX agnostic component

--- packages/lumx-react/jest/transform.js
+++ packages/lumx-react/jest/transform.js
@@ -2,5 +2,9 @@ const CONFIGS = require('../../../configs');
 
 module.exports = require('babel-jest').default.createTransformer({
     plugins: CONFIGS.babel.plugins,
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/react', '@babel/preset-typescript'],
+    presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['@babel/react', { runtime: 'automatic' }],
+        '@babel/preset-typescript'
+    ],
 });
