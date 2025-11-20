4111ec0755322e30908a2f8335f84f6687e14990
Juani Galán <juan@lumapps.com>
2025-11-20 13:48:12 +0100

    chore(lumx): drop support for React 16

--- dev-packages/eslint-config-lumapps/index.js
+++ dev-packages/eslint-config-lumapps/index.js
@@ -11,6 +11,7 @@ module.exports = {
         'airbnb/hooks',
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
         'plugin:prettier/recommended',
     ],
     parser: '@typescript-eslint/parser',
