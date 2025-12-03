0a696570bdbdaf91d2a1b480840f0e77acb3b9d5
Guillaume Cornut <gcornut@users.noreply.github.com>
2025-12-03 17:48:42 +0100

    chore(lumx-core): split event utils into modules

---
+++ packages/lumx-core/src/js/utils/events/keyboard.ts
@@ -0,0 +1,54 @@
+import type { KeyboardEvent as ReactKeyboardEvent } from 'react';
+
+type KeyboardEventHandler<E extends KeyboardEvent | ReactKeyboardEvent> = (event: E) => void;
+
+/**
+ * Make sure the pressed key is the enter key before calling the callback.
+ *
+ * @param  handler The handler to call on enter/return press.
+ * @return The decorated function.
+ */
+export function onEnterPressed<E extends KeyboardEvent | ReactKeyboardEvent>(
+    handler: KeyboardEventHandler<E>,
+): KeyboardEventHandler<E> {
+    return (evt) => {
+        if (evt.key !== 'Enter') {
+            return;
+        }
+        handler(evt);
+    };
+}
+
+/**
+ * Make sure the pressed key is the escape key before calling the callback.
+ *
+ * @param  handler The handler to call on enter/return press.
+ * @return The decorated function.
+ */
+export function onEscapePressed<E extends KeyboardEvent | ReactKeyboardEvent>(
+    handler: KeyboardEventHandler<E>,
+): KeyboardEventHandler<E> {
+    return (evt) => {
+        if (evt.key !== 'Escape') {
+            return;
+        }
+        handler(evt);
+    };
+}
+
+/**
+ * Handle button key pressed (Enter + Space).
+ *
+ * @param  handler The handler to call.
+ * @return The decorated function.
+ */
+export function onButtonPressed<E extends KeyboardEvent | ReactKeyboardEvent>(
+    handler: KeyboardEventHandler<E>,
+): KeyboardEventHandler<E> {
+    return (evt) => {
+        if (evt.key !== 'Enter' && evt.key !== ' ') {
+            return;
+        }
+        handler(evt);
+    };
+}
