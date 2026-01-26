7335656f2a8cda0ee0e6f8745dfa2420d7f33d16
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-01-26 20:48:34 +0100

    chore(deps): update to storybook v10

--- packages/lumx-react/.storybook/main.ts
+++ packages/lumx-react/.storybook/main.ts
@@ -1,17 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
-import postcss from './postcss.config';
+import path from 'node:path';
+
+const importUrl = new URL(import.meta.url);
+const postcss = path.join('../..', importUrl.pathname, 'configs');
 
 const config: StorybookConfig = {
-    framework: '@storybook/react-vite',
-    addons: [
-        '@storybook/addon-a11y',
-        '@storybook/addon-docs',
-        '@chromatic-com/storybook',
-    ],
+    addons: ['@storybook/addon-a11y', '@chromatic-com/storybook'],
     stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
     staticDirs: ['../../site-demo/static/'],
-
+    framework: '@storybook/react-vite',
     async viteFinal(config) {
         return mergeConfig(config, {
             css: { postcss },
