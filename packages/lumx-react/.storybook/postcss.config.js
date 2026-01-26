cfe5edff5f16dbdb3159ec185d3affb829fb87d0
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-01-26 20:48:34 +0100

    chore(deps): update to storybook v10

--- packages/lumx-react/.storybook/postcss.config.js
+++
@@ -1,1 +0,0 @@
-../../../configs/postcss.config.js
--- packages/lumx-vue/.storybook/main.ts
+++ packages/lumx-vue/.storybook/main.ts
@@ -1,25 +1,19 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
 import { mergeConfig } from 'vite';
+import path from 'node:path';
 
-import postcss from './postcss.config';
+const importUrl = new URL(import.meta.url);
+const postcss = path.join('../..', importUrl.pathname, 'configs');
 
 const config: StorybookConfig = {
-    addons: [
-        '@storybook/addon-a11y',
-        '@storybook/addon-docs',
-        '@chromatic-com/storybook',
-    ],
+    addons: ['@storybook/addon-a11y', '@chromatic-com/storybook'],
     stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    framework: '@storybook/vue3-vite',
-    docs: false,
+    framework: {
+        name: '@storybook/vue3-vite',
+        options: { docgen: false },
+    },
     async viteFinal(config) {
-        const vueDocgenIndex = config.plugins?.findIndex(({ name }) => name === 'storybook:vue-docgen-plugin')
-        if (vueDocgenIndex !== -1) {
-            config.plugins?.splice(vueDocgenIndex, 1);
-        }
-
         return mergeConfig(config, {
-            optimizeDeps: { include: ['@lumx/icons'] },
             css: { postcss },
         });
     },
