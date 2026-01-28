30b4751e1c0fa12f48c4078b1898ddc686cebd86
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-01-28 13:53:40 +0100

    chore(cd): rework storybook PR deploy for react and vue

--- .github/actions/utils.js
+++ .github/actions/utils.js
@@ -12,10 +12,11 @@ async function getShortSHA(gitRef) {
 /**
  * Generate StoryBook URL for the given git ref.
  */
-async function getStoryBookURL(gitRef) {
-    const { project_id } = require('../../configs/chromatic');
+async function getStoryBookURL(gitRef, package = 'lumx-react') {
+    const { projectId } = require(`./packages/${package}/chromatic.config.json`);
+    const cleanProjectId = projectId.replace('Project:', '');
     const shortSHA = await getShortSHA(gitRef);
-    return `https://${shortSHA}--${project_id}.chromatic.com/`;
+    return `https://${shortSHA}--${cleanProjectId}.chromatic.com/`;
 }
 
 module.exports = { getStoryBookURL, getShortSHA };
