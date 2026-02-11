d230240c5ca9edba6b36300e18495ffb665c701d
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-11 18:12:38 +0100

    chore(site-demo): migrate switch demos to vue

---
+++ packages/site-demo/content/product/components/switch/vue/demo1.vue
@@ -0,0 +1,18 @@
+<template>
+    <Switch :is-checked="state" :theme="theme" @change="state = $event"> Default </Switch>
+
+    <Switch :is-checked="state" :theme="theme" helper="Helper text" @change="state = $event"> With helper </Switch>
+
+    <Switch :is-disabled="true" :is-checked="state" :theme="theme" @change="state = $event"> Disabled </Switch>
+
+    <Switch :is-checked="true" :is-disabled="true" :theme="theme"> Disabled checked </Switch>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Switch, type Theme } from '@lumx/vue';
+
+defineProps<{ theme?: Theme }>();
+
+const state = ref(false);
+</script>
