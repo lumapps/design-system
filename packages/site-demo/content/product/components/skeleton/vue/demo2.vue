fb4755cb328044a0edaecc51aa260897c14be7df
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-11 18:12:23 +0100

    chore(site-demo): migrate skeleton demos to vue

---
+++ packages/site-demo/content/product/components/skeleton/vue/demo2.vue
@@ -0,0 +1,11 @@
+<template>
+    <SkeletonRectangle :theme="theme" width="xl" aspect-ratio="horizontal" />
+    <SkeletonRectangle :theme="theme" width="xl" height="m" variant="rounded" />
+    <SkeletonRectangle :theme="theme" width="m" height="s" variant="pill" />
+    <SkeletonRectangle :theme="theme" width="l" height="s" variant="pill" />
+</template>
+
+<script setup lang="ts">
+import { SkeletonRectangle, type Theme } from '@lumx/vue';
+defineProps<{ theme?: Theme }>();
+</script>
---
+++ packages/site-demo/content/product/components/skeleton/vue/demo3.vue
@@ -0,0 +1,51 @@
+<template>
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="display1" :width="230" />
+        <h1 class="lumx-typography-display1">Display 1</h1>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="headline" :width="230" />
+        <h2 class="lumx-typography-headline">Headline</h2>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="title" :width="230" />
+        <h3 class="lumx-typography-title">Title</h3>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="subtitle2" :width="230" />
+        <h4 class="lumx-typography-subtitle2">Subtitle 2</h4>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="subtitle1" :width="230" />
+        <h5 class="lumx-typography-subtitle1">Subtitle 1</h5>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="body2" :width="230" />
+        <p class="lumx-typography-body2">Body 2</p>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="body1" :width="230" />
+        <p class="lumx-typography-body1">Body 1</p>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="caption" :width="230" />
+        <p class="lumx-typography-caption">Caption</p>
+    </FlexBox>
+
+    <FlexBox orientation="horizontal" gap="huge">
+        <SkeletonTypography :theme="theme" typography="overline" :width="230" />
+        <p class="lumx-typography-overline">Overline</p>
+    </FlexBox>
+</template>
+
+<script setup lang="ts">
+import { FlexBox, SkeletonTypography, type Theme } from '@lumx/vue';
+defineProps<{ theme?: Theme }>();
+</script>
