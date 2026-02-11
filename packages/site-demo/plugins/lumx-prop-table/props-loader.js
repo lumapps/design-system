66b519a08df714913683974db503704fa2970f4a
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-11 22:05:04 +0100

    chore(site-demo): isolate react specific docgen

--- packages/site-demo/plugins/lumx-prop-table/props-loader.js
+++ packages/site-demo/plugins/lumx-prop-table/props-loader.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const { createProject, parseComponent } = require('./docgen.js');
+const { createProject } = require('./docgen.js');
+const { parseReactComponent } = require('./react-docgen.js');
 
 const ROOT_PATH = path.resolve(__dirname, '../../../..');
 const LUMX_REACT_PATH = path.resolve(ROOT_PATH, 'packages/lumx-react');
 
-// Cache project instance at module level
-let project = null;
+// Cache project instances at module level
+let reactProject = null;
 
 /**
  * LumX docs prop table webpack loader
@@ -19,15 +20,15 @@ function lumxDocsWebpackLoader() {
     const filePath = this.resourcePath;
 
     try {
-        if (!project) {
-            project = createProject(LUMX_REACT_PATH);
+        if (!reactProject) {
+            reactProject = createProject(LUMX_REACT_PATH);
         }
 
         // Add dependency so webpack watches the component file
         this.addDependency(filePath);
 
         // Parse
-        const result = parseComponent(project, filePath);
+        const result = parseReactComponent(reactProject, filePath);
 
         if (!result) {
             throw new Error('Could not load prop docs for component ' + filePath);
