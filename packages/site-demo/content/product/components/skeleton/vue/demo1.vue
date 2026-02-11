7bbe5a0fc814e167558008ba6b9e43a0b61bd19c
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-11 18:12:23 +0100

    chore(site-demo): migrate skeleton demos to vue

---
+++ packages/site-demo/content/product/components/skeleton/vue/demo1.vue
@@ -0,0 +1,10 @@
+<template>
+    <SkeletonCircle :theme="theme" size="m" />
+    <SkeletonCircle :theme="theme" size="l" />
+    <SkeletonCircle :theme="theme" size="xl" />
+</template>
+
+<script setup lang="ts">
+import { SkeletonCircle, type Theme } from '@lumx/vue';
+defineProps<{ theme?: Theme }>();
+</script>
---
+++ packages/site-demo/content/product/components/skeleton/vue/demo4.vue
@@ -0,0 +1,15 @@
+<template>
+    <FlexBox orientation="horizontal" gap="big">
+        <SkeletonCircle :theme="theme" size="m" />
+
+        <div :style="{ width: '200px' }">
+            <SkeletonTypography :theme="theme" typography="body1" />
+            <SkeletonTypography :theme="theme" typography="caption" width="60%" />
+        </div>
+    </FlexBox>
+</template>
+
+<script setup lang="ts">
+import { FlexBox, SkeletonCircle, SkeletonTypography, type Theme } from '@lumx/vue';
+defineProps<{ theme?: Theme }>();
+</script>
