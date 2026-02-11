95cac0b17e8226bd2c90c88c58e5c90b4d8a365c
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-11 18:11:52 +0100

    chore(site-demo): migrate radio-button demos to vue

---
+++ packages/site-demo/content/product/components/radio-button/vue/demo1.vue
@@ -0,0 +1,42 @@
+<template>
+    <RadioGroup>
+        <RadioButton
+            :is-checked="value === 'lorem'"
+            label="Radio button"
+            name="test1"
+            :theme="theme"
+            value="lorem"
+            @change="value = $event"
+        />
+
+        <RadioButton
+            :is-checked="value === 'ipsum'"
+            helper="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere faucibus efficitur."
+            label="Radio button with help"
+            name="test1"
+            :theme="theme"
+            value="ipsum"
+            @change="value = $event"
+        />
+
+        <RadioButton
+            :is-checked="value === 'dolor'"
+            helper="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere faucibus efficitur."
+            label="Disabled radio button with help"
+            name="test1"
+            :theme="theme"
+            value="dolor"
+            @change="value = $event"
+            :is-disabled="true"
+        />
+    </RadioGroup>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RadioButton, RadioGroup, type Theme } from '@lumx/vue';
+
+defineProps<{ theme?: Theme }>();
+
+const value = ref<string | undefined>('lorem');
+</script>
