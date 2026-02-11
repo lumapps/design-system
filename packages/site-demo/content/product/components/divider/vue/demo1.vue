99fd78c8d49bac3550a5f7faa214d88137666c1f
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-11 18:11:20 +0100

    chore(site-demo): migrate divider demos to vue

---
+++ packages/site-demo/content/product/components/divider/vue/demo1.vue
@@ -0,0 +1,8 @@
+<template>
+    <Divider :theme="theme" class="lumx-spacing-margin-vertical-big" />
+</template>
+
+<script setup lang="ts">
+import { Divider, type Theme } from '@lumx/vue';
+defineProps<{ theme?: Theme }>();
+</script>
