5b0e89842b13d6c4d9ebee260cdf016c72d7081f
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-12 10:18:25 +0100

    chore(lumx-vue): setup vue-tsc type checking

--- packages/lumx-vue/src/components/button/Stories/ButtonGroupVariants.vue
+++ packages/lumx-vue/src/components/button/Stories/ButtonGroupVariants.vue
@@ -1,17 +1,18 @@
 <template>
     <ButtonGroup>
-        <Button :size="$attrs.size" :emphasis="$attrs.emphasis" :theme="$attrs.theme">Button</Button>
-        <IconButton
-            :size="$attrs.size"
-            :emphasis="$attrs.emphasis"
-            :theme="$attrs.theme"
-            label="IconButton"
-            :icon="mdiMenuDown"
-        />
+        <Button v-bind="attrs">Button</Button>
+        <IconButton v-bind="attrs" label="IconButton" :icon="mdiMenuDown" />
     </ButtonGroup>
 </template>
 
 <script setup lang="ts">
 import { ButtonGroup, Button, IconButton } from '@lumx/vue';
 import { mdiMenuDown } from '@lumx/icons';
+import { useAttrsWithoutHandlers } from '@lumx/vue/stories/utils/useAttrsWithoutHandlers';
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+const attrs = useAttrsWithoutHandlers();
 </script>
