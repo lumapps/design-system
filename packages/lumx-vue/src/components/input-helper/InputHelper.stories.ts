270b65945965e4048984efce5e1963071e4cca16
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-17 19:17:17 +0100

    chore(input-helper): improve stories code share between react and vue

--- packages/lumx-vue/src/components/input-helper/InputHelper.stories.ts
+++ packages/lumx-vue/src/components/input-helper/InputHelper.stories.ts
@@ -1,15 +1,11 @@
-import { withWrapper } from '@lumx/vue/stories/decorators/withWrapper';
 import { withCombinations } from '@lumx/vue/stories/decorators/withCombinations';
-import { withRender } from '@lumx/vue/stories/utils/withRender';
 import { setup } from '@lumx/core/js/components/InputHelper/Stories';
 
 import { InputHelper } from '@lumx/vue';
-import InputHelperDefaultVue from './Stories/InputHelperDefault.vue';
 
 const { meta, ...stories } = setup({
     component: InputHelper,
-    render: withRender({ InputHelperDefaultVue }, '{{ args.children }}'),
-    decorators: { withWrapper, withCombinations },
+    decorators: { withCombinations },
 });
 
 export default {
--- packages/lumx-vue/src/components/input-helper/Stories/InputHelperDefault.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <InputHelper v-bind="$attrs">
-        <slot />
-    </InputHelper>
-</template>
-
-<script setup lang="ts">
-import { InputHelper } from '@lumx/vue';
-</script>
