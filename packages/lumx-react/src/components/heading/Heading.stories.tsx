519b472487072caf8db8c9a37b3306be53d89e05
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-17 19:17:08 +0100

    chore(heading): improve stories code share between react and vue

--- packages/lumx-react/src/components/heading/Heading.stories.tsx
+++ packages/lumx-react/src/components/heading/Heading.stories.tsx
@@ -1,40 +1,12 @@
 import { withCombinations } from '@lumx/react/stories/decorators/withCombinations';
-import { withWrapper } from '@lumx/react/stories/decorators/withWrapper';
 import { setup } from '@lumx/core/js/components/Heading/Stories';
 
 import { Heading, HeadingLevelProvider } from '.';
 
 const { meta, ...stories } = setup({
     component: Heading,
-    decorators: { withWrapper, withCombinations },
-    overrides: {
-        NestedHeadingLevelProvider: {
-            render: () => (
-                <>
-                    {/* This will render a h1 */}
-                    <Heading>First level</Heading>
-                    <HeadingLevelProvider>
-                        {/* This will render a h2 */}
-                        <Heading>Second Level</Heading>
-                        <HeadingLevelProvider>
-                            {/* This will render a h3 */}
-                            <Heading>Third Level</Heading>
-                            {/* This will also render a h3 */}
-                            <Heading>Other Third Level</Heading>
-                            <HeadingLevelProvider>
-                                {/* This will render a h4 */}
-                                <Heading>Fourth Level</Heading>
-                                <HeadingLevelProvider>
-                                    {/* This will render a h5 */}
-                                    <Heading>Fifth Level</Heading>
-                                </HeadingLevelProvider>
-                            </HeadingLevelProvider>
-                        </HeadingLevelProvider>
-                    </HeadingLevelProvider>
-                </>
-            ),
-        },
-    },
+    components: { HeadingLevelProvider },
+    decorators: { withCombinations },
 });
 
 export default {
--- packages/lumx-vue/src/components/heading/Heading.stories.ts
+++ packages/lumx-vue/src/components/heading/Heading.stories.ts
@@ -1,19 +1,12 @@
-import { withWrapper } from '@lumx/vue/stories/decorators/withWrapper';
 import { withCombinations } from '@lumx/vue/stories/decorators/withCombinations';
-import { withRender } from '@lumx/vue/stories/utils/withRender';
 import { setup } from '@lumx/core/js/components/Heading/Stories';
 
-import { Heading } from '.';
-import HeadingDefaultVue from './Stories/HeadingDefault.vue';
-import HeadingNestedHeadingLevelProviderVue from './Stories/HeadingNestedHeadingLevelProvider.vue';
+import { Heading, HeadingLevelProvider } from '@lumx/vue';
 
 const { meta, ...stories } = setup({
     component: Heading,
-    render: withRender({ HeadingDefaultVue }, '{{ args.children }}'),
-    decorators: { withWrapper, withCombinations },
-    overrides: {
-        NestedHeadingLevelProvider: { render: withRender({ HeadingNestedHeadingLevelProviderVue }) },
-    },
+    components: { HeadingLevelProvider },
+    decorators: { withCombinations },
 });
 
 export default {
