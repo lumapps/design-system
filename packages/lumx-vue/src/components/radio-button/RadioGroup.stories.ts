df8cc037ab1830b747027c294b540f8525a8bffc
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-17 09:46:55 +0100

    chore(radio-group): improve vue+react stories

--- packages/lumx-vue/src/components/radio-button/RadioGroup.stories.ts
+++ packages/lumx-vue/src/components/radio-button/RadioGroup.stories.ts
@@ -1,13 +1,11 @@
 import { withRender } from '@lumx/vue/stories/utils/withRender';
 
 import { RadioGroup } from '@lumx/vue';
-import RadioGroupDefaultVue from './Stories/RadioGroupDefault.vue';
 import RadioGroupSimpleVue from './Stories/RadioGroupSimple.vue';
 
 export default {
     title: 'LumX components/radio-button/Radio Group',
     component: RadioGroup,
-    render: withRender({ RadioGroupDefaultVue }),
 };
 
 /**
