6f2bfca5daa29775366b3d667b364e7e0b2f146e
Juani Galán <juan@lumapps.com>
2026-02-17 11:15:32 +0100

    qwe

--- packages/lumx-react/src/components/combobox/index.ts
+++ packages/lumx-react/src/components/combobox/index.ts
@@ -8,4 +8,12 @@ export * from './components/ComboboxOptionAction';
 export * from './components/ComboboxOptionMoreInfo';
 export * from './components/ComboboxSection';
 export * from './components/Combobox';
-export type * from './types';
+export type {
+    ComboboxOptionProps,
+    ComboboxProps,
+    BaseSelectProps,
+    MultipleSelection,
+    OnComboboxInputChange,
+    OnComboboxSelect,
+    SingleSelection,
+} from './types';
--- packages/lumx-react/src/utils/index.ts
+++ packages/lumx-react/src/utils/index.ts
@@ -8,4 +8,4 @@ export { A11YLiveMessage, type A11YLiveMessageProps } from './A11YLiveMessage';
 export { Portal, type PortalProps, type PortalInit, PortalProvider, type PortalProviderProps } from './Portal';
 export { DisabledStateProvider, useDisabledStateContext } from './disabled';
 export * from './moving-focus';
-export * from '../components/combobox/hooks/useCombobox';
+export { useCombobox } from '../components/combobox/hooks/useCombobox';
