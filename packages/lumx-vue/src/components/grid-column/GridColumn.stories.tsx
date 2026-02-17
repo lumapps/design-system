adeee64dc21da5a07e24a40fc01fbb932640bf3c
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-17 19:17:03 +0100

    chore(grid-column): improve stories code share between react and vue

--- packages/lumx-vue/src/components/grid-column/GridColumn.stories.tsx
+++ packages/lumx-vue/src/components/grid-column/GridColumn.stories.tsx
@@ -1,21 +1,8 @@
-import range from 'lodash/range';
 import { GridColumn } from '@lumx/vue';
-import { loremIpsum } from '@lumx/core/stories/utils/lorem';
 import { setup } from '@lumx/core/js/components/GridColumn/GridColumnStories';
-
-const genericBlockStyle = { border: '1px solid red', padding: '2px 8px' };
-
-const getItems = (nb: number) =>
-    range(nb).map((key) => (
-        <div key={key} style={genericBlockStyle}>
-            <h2>Column {key}</h2>
-            <p>{loremIpsum('tiny')}</p>
-        </div>
-    ));
 
 const { meta, ...stories } = setup({
     component: GridColumn,
-    render: ({ nbItems = 5, ...props }: any) => <GridColumn {...props}>{getItems(nbItems)}</GridColumn>,
 });
 
 export default {
