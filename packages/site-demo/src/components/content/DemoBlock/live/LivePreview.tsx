a90072bdfb99077598286b1ca3c872386b000e14
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-25 21:38:11 +0100

    chore(site-demo): contain portals in demo component

--- packages/site-demo/src/components/content/DemoBlock/live/LivePreview.tsx
+++ packages/site-demo/src/components/content/DemoBlock/live/LivePreview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { FlexBox, HasClassName } from '@lumx/react';
+import { PortalInit, PortalProvider } from '@lumx/react/utils';
 import { classNames } from '@lumx/core/js/utils';
 
 import { useLiveContext } from './LiveContext';
@@ -15,13 +16,18 @@ export function LivePreview({ className }: HasClassName) {
     const { demo, theme, isEditMode, flexBoxProps } = useLiveContext();
     const DemoComponent = demo?.react?.default;
     const demoProps: any = isEditMode ? { inert: 'true', style: { visibility: 'hidden' } } : undefined;
+    const portalRef = React.useRef<HTMLDivElement>(null);
+    const initPortal = React.useCallback<PortalInit>(() => ({ container: portalRef.current || undefined }), []);
 
     return (
         <div className={block([className as string])}>
             {DemoComponent ? (
                 <div className={element('static')} {...demoProps}>
                     <FlexBox {...flexBoxProps}>
-                        <DemoComponent theme={theme} />
+                        <PortalProvider value={initPortal}>
+                            <DemoComponent theme={theme} />
+                            <div ref={portalRef} />
+                        </PortalProvider>
                     </FlexBox>
                 </div>
             ) : null}
