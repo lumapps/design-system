fe2c77827f71955c58398bd821446ec757d67468
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-02-25 10:51:19 +0100

    chore(vue): remove unnecessary const exports

--- packages/lumx-vue/src/components/grid-column/index.ts
+++ packages/lumx-vue/src/components/grid-column/index.ts
@@ -1,3 +1,2 @@
 export { default as GridColumn } from './GridColumn';
 export type { GridColumnProps, GridColumnGapSize } from './GridColumn';
-export { CLASSNAME as GRID_COLUMN_CLASSNAME, COMPONENT_NAME as GRID_COLUMN_COMPONENT_NAME } from './GridColumn';
--- packages/lumx-vue/src/components/switch/index.ts
+++ packages/lumx-vue/src/components/switch/index.ts
@@ -1,3 +1,2 @@
 export { default as Switch } from './Switch';
 export type { SwitchProps } from './Switch';
-export { CLASSNAME, COMPONENT_NAME, DEFAULT_PROPS } from './Switch';
