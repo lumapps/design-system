541b9969992f8b59fae1125ac717f53227698bb8
Guillaume Cornut <gcornut@users.noreply.github.com>
2026-06-24 16:50:19 +0200

    chore(ci): optimize check-affected checkout and setup

--- .github/actions/check-affected/index.mjs
+++ .github/actions/check-affected/index.mjs
@@ -1,22 +1,17 @@
 import { execSync } from 'node:child_process';
 import { createProjectGraphAsync } from 'nx/src/project-graph/project-graph.js';
 import { filterAffected } from 'nx/src/project-graph/affected/affected-project-graph.js';
-import { WholeFileChange } from 'nx/src/project-graph/file-utils.js';
+import { calculateFileChanges } from 'nx/src/project-graph/file-utils.js';
+import { parseFiles } from 'nx/src/utils/command-line-utils.js';
 import * as core from '@actions/core';
 
 async function main() {
-    const changedFiles = execSync('git diff --name-only origin/master...HEAD --diff-filter=AM')
-        .toString()
-        .trim()
-        .split('\n')
-        .filter(Boolean);
-
     const graph = await createProjectGraphAsync();
 
-    const touchedFiles = changedFiles.map((file) => ({
-        file,
-        getChanges: () => [new WholeFileChange()],
-    }));
+    const base = execSync('git merge-base origin/master HEAD').toString().trim();
+    const nxArgs = { base, head: 'HEAD' };
+    const { files: changedFiles } = parseFiles(nxArgs);
+    const touchedFiles = changedFiles.length > 0 ? calculateFileChanges(changedFiles, nxArgs) : [];
 
     const affectedGraph =
         changedFiles.length > 0 ? await filterAffected(graph, touchedFiles) : graph;
